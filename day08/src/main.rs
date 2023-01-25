37c4d05e86ecd3cf44d10818ea88c634e1b6d8b9
David Stone <david@stabbylambda.com>
2023-01-24 16:29:28 -0800

    Day 8

---
+++ day08/src/main.rs
@@ -0,0 +1,140 @@
+use common::get_raw_input;
+use ndarray::{prelude, s, Array2};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, newline, u32 as nom_u32},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
+
+const WIDTH: usize = 50;
+const HEIGHT: usize = 6;
+
+fn main() {
+    let input = get_raw_input();
+    let input = parse(&input);
+
+    let score = problem1(&input, WIDTH, HEIGHT);
+    println!("problem 1 score: {score}");
+}
+
+type Input = Vec<Instruction>;
+
+#[derive(Debug)]
+enum Instruction {
+    Rectangle { width: usize, height: usize },
+    RotateRow { index: usize, amount: usize },
+    RotateColumn { index: usize, amount: usize },
+}
+
+fn parse(input: &str) -> Input {
+    let result: IResult<&str, Input> = separated_list1(
+        newline,
+        alt((
+            map(
+                preceded(tag("rect "), separated_pair(nom_u32, char('x'), nom_u32)),
+                |(width, height)| Instruction::Rectangle {
+                    width: width as usize,
+                    height: height as usize,
+                },
+            ),
+            map(
+                preceded(
+                    tag("rotate row y="),
+                    separated_pair(nom_u32, tag(" by "), nom_u32),
+                ),
+                |(index, amount)| Instruction::RotateRow {
+                    index: index as usize,
+                    amount: amount as usize,
+                },
+            ),
+            map(
+                preceded(
+                    tag("rotate column x="),
+                    separated_pair(nom_u32, tag(" by "), nom_u32),
+                ),
+                |(index, amount)| Instruction::RotateColumn {
+                    index: index as usize,
+                    amount: amount as usize,
+                },
+            ),
+        )),
+    )(input);
+
+    result.unwrap().1
+}
+
+fn problem1(input: &Input, width: usize, height: usize) -> u32 {
+    let mut arr: Array2<bool> = Array2::default((height, width));
+
+    for instruction in input {
+        match instruction {
+            Instruction::Rectangle { width, height } => {
+                // get the mutable slice in the upper left and fill with true
+                arr.slice_mut(s![0..*height, 0..*width]).fill(true);
+            }
+            Instruction::RotateRow { index, amount } => {
+                let shifted: Vec<bool> = arr
+                    .row(*index)
+                    .into_iter()
+                    .copied()
+                    .cycle()
+                    .skip(width - *amount)
+                    .take(width)
+                    .collect();
+
+                for (idx, v) in arr.row_mut(*index).indexed_iter_mut() {
+                    *v = shifted[idx];
+                }
+            }
+            Instruction::RotateColumn { index, amount } => {
+                let shifted: Vec<bool> = arr
+                    .column(*index)
+                    .into_iter()
+                    .copied()
+                    .cycle()
+                    .skip(height - *amount)
+                    .take(height)
+                    .collect();
+
+                for (idx, v) in arr.column_mut(*index).indexed_iter_mut() {
+                    *v = shifted[idx];
+                }
+            }
+        }
+
+        for x in arr.rows() {
+            for y in x {
+                print!(
+                    "{}",
+                    match y {
+                        true => "█",
+                        false => " ",
+                    }
+                );
+            }
+
+            println!();
+        }
+        println!();
+    }
+
+    arr.fold(0, |acc, x| acc + u32::from(*x))
+}
+
+#[cfg(test)]
+mod test {
+    use common::test::get_raw_input;
+
+    use crate::{parse, problem1};
+    #[test]
+    fn first() {
+        let input = get_raw_input();
+        let input = parse(&input);
+        let result = problem1(&input, 7, 3);
+        assert_eq!(result, 6)
+    }
+}
