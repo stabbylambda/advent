76804b7f52849eee0990c8e871e4bf8d61f0db4c
David Stone <david@stabbylambda.com>
2023-01-31 14:40:04 -0800

    Day 13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,9 +1,12 @@
-use common::get_raw_input;
-use nom::IResult;
+use std::collections::HashSet;
+
+use common::{
+    dijkstra::{shortest_path, Edge},
+    map::Map,
+};
 
 fn main() {
-    let input = get_raw_input();
-    let input = parse(&input);
+    let input = Input::new(1364, (31, 39));
 
     let score = problem1(&input);
     println!("problem 1 score: {score}");
@@ -12,40 +15,129 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
-
-fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
-
-    result.unwrap().1
+struct Input {
+    favorite_number: usize,
+    x: usize,
+    y: usize,
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+impl Input {
+    fn new(favorite_number: usize, (x, y): (usize, usize)) -> Self {
+        Input {
+            favorite_number,
+            x,
+            y,
+        }
+    }
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+const PADDING: usize = 60;
+fn generate_maze(input: &Input) -> Map<bool> {
+    let max_x = PADDING;
+    let max_y = PADDING;
+
+    let mut result = vec![vec![false; max_x + 1]; max_y];
+    (0..max_x).for_each(|x| {
+        (0..max_y).for_each(|y| {
+            let mut z = x * x + 3 * x + 2 * x * y + y + y * y;
+            z += input.favorite_number;
+            // odd is a wall
+            result[y][x] = z.count_ones() % 2 != 0;
+        });
+    });
+
+    Map::new(result)
+}
+
+fn print_maze(maze: &Map<bool>, seen: &HashSet<(usize, usize)>) {
+    for (y, row) in maze.points.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            match cell {
+                true => print!("#"),
+                false if seen.contains(&(x, y)) => print!("O"),
+                false => print!("."),
+            }
+        }
+        println!();
+    }
+}
+
+fn get_edges(maze: &Map<bool>) -> Vec<Vec<Edge>> {
+    maze.into_iter()
+        .map(|square| {
+            // walls have no edges
+            if *square.data {
+                return vec![];
+            }
+
+            square
+                .neighbors()
+                .iter()
+                .filter_map(|n| {
+                    if *n.data {
+                        return None;
+                    }
+
+                    Some(Edge {
+                        node: n.get_grid_index(),
+                        cost: 1,
+                    })
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn problem1(input: &Input) -> usize {
+    let maze = generate_maze(input);
+    let edges = get_edges(&maze);
+
+    shortest_path(
+        &edges,
+        maze.get((1, 1)).get_grid_index(),
+        maze.get((input.x, input.y)).get_grid_index(),
+    )
+    .unwrap()
+}
+
+fn problem2(input: &Input) -> usize {
+    let maze = generate_maze(input);
+    let edges = get_edges(&maze);
+
+    let mut seen: HashSet<(usize, usize)> = HashSet::new();
+
+    let start = maze.get((1, 1)).get_grid_index();
+
+    for x in 0..PADDING {
+        for y in (0..(PADDING - x)).rev() {
+            if let Some(dist) = shortest_path(&edges, start, maze.get((x, y)).get_grid_index()) {
+                if dist <= 50 {
+                    seen.insert((x, y));
+                }
+            }
+        }
+    }
+
+    print_maze(&maze, &seen);
+    seen.len()
 }
 
 #[cfg(test)]
 mod test {
-    use common::test::get_raw_input;
 
-    use crate::{parse, problem1, problem2};
+    use crate::{problem1, problem2, Input};
+
     #[test]
     fn first() {
-        let input = get_raw_input();
-        let input = parse(&input);
+        let input = Input::new(10, (7, 4));
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 11)
     }
 
     #[test]
     fn second() {
-        let input = get_raw_input();
-        let input = parse(&input);
+        let input = Input::new(10, (7, 4));
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 151)
     }
 }
