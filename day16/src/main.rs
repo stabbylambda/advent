ec12984e3aff7117f2ce890424ca79033c358be3
David Stone <david@stabbylambda.com>
2023-02-01 13:37:52 -0800

    Day 16

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,51 +1,83 @@
-use common::get_raw_input;
-use nom::IResult;
-
 fn main() {
-    let input = get_raw_input();
-    let input = parse(&input);
+    let input = Disc {
+        length: 272,
+        initial: "01000100010010111",
+    };
 
-    let score = problem1(&input);
+    let score = problem(&input);
     println!("problem 1 score: {score}");
 
-    let score = problem2(&input);
+    let input = Disc {
+        length: 35651584,
+        initial: "01000100010010111",
+    };
+
+    let score = problem(&input);
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
-
-fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
-
-    result.unwrap().1
+type Input<'a> = Disc<'a>;
+struct Disc<'a> {
+    length: usize,
+    initial: &'a str,
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
-}
+/* This is a garbage solution that is slow for problem 2. The better solution is
+ * https://www.reddit.com/r/adventofcode/comments/5ititq/2016_day_16_c_how_to_tame_your_dragon_in_under_a/
+ */
+fn problem(input: &Input) -> String {
+    let mut data = String::with_capacity(input.length);
+    data.push_str(input.initial);
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
-}
+    // generate the string
+    while data.len() < input.length {
+        let b: String = data
+            .chars()
+            .rev()
+            // this could probably be done using bitwise not, but our final doesn't fit in a u128
+            // let's see if we can cheat this way
+            .map(|c| match c {
+                '0' => '1',
+                '1' => '0',
+                _ => unreachable!(),
+            })
+            .collect();
 
-#[cfg(test)]
-mod test {
-    use common::test::get_raw_input;
-
-    use crate::{parse, problem1, problem2};
-    #[test]
-    fn first() {
-        let input = get_raw_input();
-        let input = parse(&input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
+        data.push_str(&format!("0{b}"));
     }
 
+    // trim down to the amount we need to fill
+    let mut v: Vec<char> = data.chars().take(input.length).collect();
+    while v.len() % 2 == 0 {
+        v = v
+            .chunks(2)
+            .map(|x| match (x[0], x[1]) {
+                ('0', '0') | ('1', '1') => '1',
+                _ => '0',
+            })
+            .collect();
+    }
+
+    v.iter().collect()
+}
+#[cfg(test)]
+mod test {
+
+    use crate::{problem, Disc};
     #[test]
-    fn second() {
-        let input = get_raw_input();
-        let input = parse(&input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+    fn first() {
+        let input = Disc {
+            length: 12,
+            initial: "110010110100",
+        };
+
+        assert_eq!(problem(&input), "100");
+
+        let input = Disc {
+            length: 20,
+            initial: "10000",
+        };
+
+        assert_eq!(problem(&input), "01100");
     }
 }
