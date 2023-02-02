5e056a4b63b5ab66d56822bacf1fcd256db5146c
David Stone <david@stabbylambda.com>
2023-02-02 14:03:12 -0800

    Day 19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,51 +1,41 @@
-use common::get_raw_input;
-use nom::IResult;
-
 fn main() {
-    let input = get_raw_input();
-    let input = parse(&input);
-
-    let score = problem1(&input);
-    println!("problem 1 score: {score}");
-
-    let score = problem2(&input);
-    println!("problem 2 score: {score}");
+    let input = 3_012_210;
+    println!("Solution 1: {}", problem1(input));
+    println!("Solution 2: {}", problem2(input as u32));
 }
 
-type Input = Vec<u32>;
+// return a number with only the most significant bit set
+fn msb(n: i32) -> i32 {
+    let mut count: i32 = 0;
+    let mut num = n;
+    while num > 0 {
+        count += 1;
+        num >>= 1;
+    }
 
-fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
-
-    result.unwrap().1
+    1 << (count - 1)
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: i32) -> i32 {
+    // https://en.wikipedia.org/wiki/Josephus_problem#Bitwise
+    !msb(input * 2) & ((input << 1) | 1)
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: u32) -> u32 {
+    let mut i = 1;
+
+    while i * 3 < input {
+        i *= 3;
+    }
+
+    input - i
 }
 
 #[cfg(test)]
 mod test {
-    use common::test::get_raw_input;
-
-    use crate::{parse, problem1, problem2};
+    use crate::problem1;
     #[test]
     fn first() {
-        let input = get_raw_input();
-        let input = parse(&input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
-    }
-
-    #[test]
-    fn second() {
-        let input = get_raw_input();
-        let input = parse(&input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(problem1(5), 3)
     }
 }
