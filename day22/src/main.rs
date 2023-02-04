43b444515e241b7ce549f19e3befed84e6f14429
David Stone <david@stabbylambda.com>
2023-02-03 16:09:49 -0800

    Day 22 part 1

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,5 +1,12 @@
 use common::get_raw_input;
-use nom::IResult;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{multispace1, newline, u32},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair, terminated, tuple},
+    IResult,
+};
 
 fn main() {
     let input = get_raw_input();
@@ -12,16 +19,59 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<Node>;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Node {
+    x: u32,
+    y: u32,
+    size: u32,
+    used: u32,
+    avail: u32,
+}
+
+impl Node {
+    fn is_empty(&self) -> bool {
+        self.used == 0
+    }
+}
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let name = preceded(
+        tag("/dev/grid/node-"),
+        separated_pair(preceded(tag("x"), u32), tag("-"), preceded(tag("y"), u32)),
+    );
+
+    let result: IResult<&str, Input> = separated_list1(
+        newline,
+        map(
+            tuple((
+                terminated(name, multispace1),
+                terminated(terminated(u32, tag("T")), multispace1),
+                terminated(terminated(u32, tag("T")), multispace1),
+                terminated(terminated(u32, tag("T")), multispace1),
+                terminated(u32, tag("%")),
+            )),
+            |((x, y), size, used, avail, _used_percent)| Node {
+                x,
+                y,
+                size,
+                used,
+                avail,
+            },
+        ),
+    )(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> usize {
+    input
+        .iter()
+        .filter(|a| !a.is_empty())
+        .flat_map(|a| input.iter().filter_map(move |b| (a != b).then_some((a, b))))
+        .filter(|(a, b)| a.used <= b.avail)
+        .count()
 }
 
 fn problem2(_input: &Input) -> u32 {
@@ -38,7 +88,7 @@ mod test {
         let input = get_raw_input();
         let input = parse(&input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 903)
     }
 
     #[test]
