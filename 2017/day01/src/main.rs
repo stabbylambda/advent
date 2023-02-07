33da1524e470c594cc3354dd29d7207a5a4f484e
David Stone <david@stabbylambda.com>
2023-02-07 15:02:44 -0800

    Setup everything

---
+++ 2017/day01/src/main.rs
@@ -0,0 +1,69 @@
+use common::nom::single_digit;
+use nom::{multi::many1, IResult};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let input = parse(input);
+
+    let score = problem1(&input);
+    println!("problem 1 score: {score}");
+
+    let score = problem2(&input);
+    println!("problem 2 score: {score}");
+}
+
+type Input = Vec<u32>;
+
+fn parse(input: &str) -> Input {
+    let result: IResult<&str, Input> = many1(single_digit)(input);
+
+    result.unwrap().1
+}
+
+fn problem1(input: &Input) -> u32 {
+    let mut v = input.clone();
+    v.push(*v.first().unwrap());
+
+    v.windows(2)
+        .map(|w| if w[0] == w[1] { w[0] } else { 0 })
+        .sum()
+}
+
+fn problem2(input: &Input) -> u32 {
+    let (first, second) = input.split_at(input.len() / 2);
+    first
+        .iter()
+        .zip(second.iter())
+        .map(|(&a, &b)| if a == b { 2 * a } else { 0 })
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{parse, problem1, problem2};
+    #[test]
+    fn first() {
+        let cases = [("1122", 3), ("1111", 4), ("1234", 0), ("91212129", 9)];
+        for (input, expected) in cases {
+            let input = parse(input);
+            let result = problem1(&input);
+            assert_eq!(result, expected)
+        }
+    }
+
+    #[test]
+    fn second() {
+        let cases = [
+            ("1212", 6),
+            ("1221", 0),
+            ("123425", 4),
+            ("123123", 12),
+            ("12131415", 4),
+        ];
+        for (input, expected) in cases {
+            let input = parse(input);
+            let result = problem2(&input);
+            assert_eq!(result, expected)
+        }
+    }
+}
