e7b1f89ba58956e8ce51416f9ec8a2fe16833bf8
David Stone <david@stabbylambda.com>
2023-02-08 12:55:43 -0800

    Day 7

--- 2017/day07/src/main.rs
+++ 2017/day07/src/main.rs
@@ -19,24 +19,52 @@ fn main() {
     let root = problem1(&input);
     println!("problem 1 score: {root}");
 
-    let score = problem2(&input, root);
+    let score = problem2(&input);
     println!("problem 2 score: {score}");
 }
 
-type Input<'a> = HashMap<&'a str, Program<'a>>;
+type Input<'a> = Program<'a>;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct Program<'a> {
     name: &'a str,
     weight: u32,
-    programs: Vec<&'a str>,
+    programs: Vec<Program<'a>>,
+}
+impl<'a> Program<'a> {
+    fn get_total_weight(&self) -> u32 {
+        self.programs
+            .iter()
+            .fold(self.weight, |acc, x| acc + x.get_total_weight())
+    }
+
+    fn all_children_balanced(&self) -> bool {
+        let set: HashSet<u32> = self.programs.iter().map(|x| x.get_total_weight()).collect();
+        set.len() == 1
+    }
 }
 
+impl<'a> PartialOrd for Program<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for Program<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.get_total_weight().cmp(&other.get_total_weight())
+    }
+}
 impl<'a> Display for Program<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} ({})", self.name, self.weight)?;
         if !self.programs.is_empty() {
-            let programs = self.programs.join(", ");
+            let programs = self
+                .programs
+                .iter()
+                .map(|x| x.name)
+                .collect::<Vec<&str>>()
+                .join(", ");
             write!(f, " -> {programs}")?;
         }
 
@@ -51,69 +79,64 @@ fn parse(input: &str) -> Input {
             tuple((
                 terminated(alpha1, tag(" ")),
                 delimited(tag("("), u32, tag(")")),
-                opt(preceded(tag(" -> "), separated_list1(tag(", "), alpha1))),
+                map(
+                    opt(preceded(tag(" -> "), separated_list1(tag(", "), alpha1))),
+                    |v| v.unwrap_or_default(),
+                ),
             )),
         ),
         |v| {
             let map: HashMap<_, _> = v
+                .clone()
                 .into_iter()
-                .map(|(name, weight, programs)| {
-                    (
-                        name,
-                        Program {
-                            name,
-                            weight,
-                            programs: programs.unwrap_or_default(),
-                        },
-                    )
-                })
+                .map(|(name, weight, programs)| (name, (weight, programs)))
                 .collect();
+            let not_root: HashSet<&str> = v
+                .iter()
+                .flat_map(|(_, _, programs)| programs.iter().cloned())
+                .collect();
+            let all: HashSet<&str> = v.iter().map(|(name, _, _)| *name).collect();
+            let root = *all.difference(&not_root).next().unwrap();
 
-            map
+            build_tree(&map, root)
         },
     )(input);
 
     result.unwrap().1
 }
 
+fn build_tree<'a>(map: &HashMap<&'a str, (u32, Vec<&'a str>)>, name: &'a str) -> Program<'a> {
+    let (weight, children) = &map[name];
+    let programs = children.iter().map(|&x| build_tree(map, x)).collect();
+
+    Program {
+        name,
+        weight: *weight,
+        programs,
+    }
+}
+
 fn problem1<'a>(input: &'a Input) -> &'a str {
-    let not_root: HashSet<&str> = input
-        .iter()
-        .flat_map(|(_name, p)| p.programs.iter().cloned())
-        .collect();
-    let all: HashSet<&str> = input.iter().map(|(&name, _p)| name).collect();
-
-    all.difference(&not_root).next().unwrap()
+    input.name
 }
 
-fn problem2(input: &Input, root: &str) -> u32 {
-    let branches: Vec<(&str, u32, u32)> = input[root]
-        .programs
-        .iter()
-        .map(|&name| (name, input[name].weight, get_weight(input, name)))
-        .collect();
+fn problem2(input: &Input) -> u32 {
+    let mut current = input;
 
-    let min_stack = branches
-        .iter()
-        .min_by_key(|(name, self_weight, total_weight)| total_weight)
-        .unwrap();
-    let max_stack = branches
-        .iter()
-        .max_by_key(|(name, self_weight, total_weight)| total_weight)
-        .unwrap();
+    let result = loop {
+        let max_child = current.programs.iter().max().unwrap();
+        let min_child = current.programs.iter().min().unwrap();
 
-    let diff = max_stack.2 - min_stack.2;
-    let would_be = max_stack.1 - diff;
-    dbg!(&branches, diff, would_be);
+        // the current node is the problem
+        if max_child.all_children_balanced() {
+            let diff = max_child.get_total_weight() - min_child.get_total_weight();
+            break max_child.weight - diff;
+        } else {
+            current = max_child;
+        }
+    };
 
-    would_be
-}
-
-fn get_weight(input: &HashMap<&str, Program>, name: &str) -> u32 {
-    let p = &input[name];
-    p.programs
-        .iter()
-        .fold(p.weight, |acc, x| acc + get_weight(input, x))
+    result
 }
 
 #[cfg(test)]
@@ -131,7 +154,7 @@ mod test {
     fn second() {
         let input = include_str!("../test.txt");
         let input = parse(input);
-        let result = problem2(&input, "tknk");
+        let result = problem2(&input);
         assert_eq!(result, 60)
     }
 }
