4f48ba87898d3bf6b1d6f87701bb617cb949adf2
David Stone <david@stabbylambda.com>
2023-02-13 10:04:55 -0800

    Day 10 part 1

--- 2017/day10/src/main.rs
+++ 2017/day10/src/main.rs
@@ -1,26 +1,45 @@
-use nom::IResult;
+use std::collections::VecDeque;
+
+use nom::{
+    bytes::complete::tag, character::complete::u32, combinator::map, multi::separated_list1,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
     let input = parse(input);
 
-    let score = problem1(&input);
+    let score = problem1(&input, 256);
     println!("problem 1 score: {score}");
 
     let score = problem2(&input);
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<usize>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = separated_list1(tag(","), map(u32, |x| x as usize))(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input, max: usize) -> usize {
+    let mut rope: VecDeque<usize> = (0..max).collect();
+    let mut current = 0;
+
+    for (skip, length) in input.iter().enumerate() {
+        /* Rotate to make the current at 0, reverse up to the length,
+        then rotate back so we're in the right spot again */
+        rope.rotate_left(current);
+        rope.make_contiguous()[0..*length].reverse();
+        rope.rotate_right(current);
+
+        // Move the current position forward by that length plus the skip size.
+        current = (current + length + skip) % max;
+    }
+
+    rope[0] * rope[1]
 }
 
 fn problem2(_input: &Input) -> u32 {
@@ -34,8 +53,8 @@ mod test {
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
+        let result = problem1(&input, 5);
+        assert_eq!(result, 12)
     }
 
     #[test]
