5a81a6fde30f70735510c2b8f03064a6008972e0
David Stone <david@stabbylambda.com>
2023-02-24 10:16:46 -0800

    Day 22

--- 2017/day22/src/main.rs
+++ 2017/day22/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use nom::{
     branch::alt,
@@ -10,98 +10,155 @@ use nom::{
 
 fn main() {
     let input = include_str!("../input.txt");
-    let input = parse(input);
+    let mut input = parse(input);
 
-    let answer = problem1(&input);
+    let answer = problem1(&mut input.clone());
     println!("problem 1 answer: {answer}");
 
-    let answer = problem2(&input);
+    let answer = problem2(&mut input);
     println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<Vec<bool>>;
+type Input = InfectedNodes;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(
-        newline,
-        many1(alt((map(char('#'), |_| true), map(char('.'), |_| false)))),
+    let result: IResult<&str, Input> = map(
+        separated_list1(
+            newline,
+            many1(alt((map(char('#'), |_| true), map(char('.'), |_| false)))),
+        ),
+        InfectedNodes::new,
     )(input);
 
     result.unwrap().1
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy)]
 enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+    Up = 0,
+    Right = 1,
+    Down = 2,
+    Left = 3,
 }
 
 impl Direction {
-    fn turn_right(&self) -> Self {
-        match self {
-            Self::Up => Self::Right,
-            Self::Right => Self::Down,
-            Self::Down => Self::Left,
-            Self::Left => Self::Up,
-        }
+    const STATES: [Self; 4] = [Self::Up, Self::Right, Self::Down, Self::Left];
+
+    fn index(&self) -> usize {
+        *self as usize
     }
+
+    fn turn_right(&self) -> Self {
+        Self::STATES[(self.index() + 1) % 4]
+    }
+
+    fn reverse(&self) -> Self {
+        Self::STATES[(self.index() + 2) % 4]
+    }
+
     fn turn_left(&self) -> Self {
-        match self {
-            Self::Up => Self::Left,
-            Self::Left => Self::Down,
-            Self::Down => Self::Right,
-            Self::Right => Self::Up,
-        }
+        Self::STATES[(self.index() + 3) % 4]
     }
 }
 
-fn problem1(input: &Input) -> u32 {
-    let bursts = 10_000;
-    let mut infection_count = 0;
-    let mut infected_nodes: HashSet<(i64, i64)> = HashSet::new();
-    let mut dir = Direction::Up;
+#[derive(Clone, Copy)]
+enum Node {
+    Clean,
+    Weakened,
+    Infected,
+    Flagged,
+}
 
-    // we start in the middle of the grid for ease of computation
-    let mut current = (input.len() as i64 / 2, input.len() as i64 / 2);
-    for (y, row) in input.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            if *cell {
-                infected_nodes.insert((x as i64, y as i64));
+#[derive(Clone)]
+struct InfectedNodes {
+    current: (i64, i64),
+    dir: Direction,
+    nodes: HashMap<(i64, i64), Node>,
+}
+
+impl InfectedNodes {
+    fn new(input: Vec<Vec<bool>>) -> Self {
+        let mut nodes = HashMap::new();
+        // we start in the middle of the grid for ease of computation
+        let current = (input.len() as i64 / 2, input.len() as i64 / 2);
+        for (y, row) in input.iter().enumerate() {
+            for (x, cell) in row.iter().enumerate() {
+                if *cell {
+                    nodes.insert((x as i64, y as i64), Node::Infected);
+                }
             }
+        }
+
+        Self {
+            current,
+            nodes,
+            dir: Direction::Up,
         }
     }
 
-    for _n in 0..bursts {
-        let current_infected = infected_nodes.contains(&current);
+    fn move_dir(&mut self) {
+        let (x, y) = self.current;
+        self.current = match self.dir {
+            Direction::Up => (x, y - 1),
+            Direction::Down => (x, y + 1),
+            Direction::Left => (x - 1, y),
+            Direction::Right => (x + 1, y),
+        };
+    }
+}
+
+fn problem1(input: &mut Input) -> u32 {
+    let mut infection_count = 0;
+
+    for _n in 0..10_000 {
+        let current_infected = input.nodes.entry(input.current).or_insert(Node::Clean);
         // execute the turn
-        dir = match current_infected {
-            true => dir.turn_right(),
-            false => dir.turn_left(),
+        input.dir = match current_infected {
+            Node::Infected => input.dir.turn_right(),
+            _ => input.dir.turn_left(),
         };
 
-        if current_infected {
-            infected_nodes.remove(&current);
-        } else {
-            // keep track of all the times we've infected a node
-            infection_count += 1;
-            infected_nodes.insert(current);
-        }
-
-        match dir {
-            Direction::Up => current.1 -= 1,
-            Direction::Down => current.1 += 1,
-            Direction::Left => current.0 -= 1,
-            Direction::Right => current.0 += 1,
+        *current_infected = match current_infected {
+            Node::Infected => Node::Clean,
+            _ => {
+                infection_count += 1;
+                Node::Infected
+            }
         };
+
+        input.move_dir();
     }
 
     infection_count
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &mut Input) -> u32 {
+    let mut infection_count = 0;
+
+    for _n in 0..10_000_000 {
+        let current_infected = input.nodes.entry(input.current).or_insert(Node::Clean);
+        // execute the turn
+        input.dir = match current_infected {
+            Node::Clean => input.dir.turn_left(),
+            Node::Weakened => input.dir,
+            Node::Infected => input.dir.turn_right(),
+            Node::Flagged => input.dir.reverse(),
+        };
+
+        *current_infected = match current_infected {
+            Node::Clean => Node::Weakened,
+            Node::Weakened => {
+                infection_count += 1;
+                Node::Infected
+            }
+            Node::Infected => Node::Flagged,
+            Node::Flagged => Node::Clean,
+        };
+
+        input.move_dir();
+    }
+
+    infection_count
 }
 
 #[cfg(test)]
@@ -110,16 +167,16 @@ mod test {
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem1(&input);
+        let mut input = parse(input);
+        let result = problem1(&mut input);
         assert_eq!(result, 5587)
     }
 
     #[test]
     fn second() {
         let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let mut input = parse(input);
+        let result = problem2(&mut input);
+        assert_eq!(result, 2511944)
     }
 }
