17ea4a633b4ef867bf4010e181313bec573d06bc
David Stone <david@stabbylambda.com>
2023-02-27 07:54:15 -0800

    2018 day 1 and 2

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -1,4 +1,12 @@
-use nom::IResult;
+use std::collections::HashSet;
+
+use nom::{
+    branch::alt,
+    character::complete::{char, i32, newline},
+    multi::separated_list1,
+    sequence::preceded,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -11,20 +19,31 @@ fn main() {
     println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<i32>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> =
+        separated_list1(newline, alt((preceded(char('+'), i32), i32)))(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> i32 {
+    input.iter().sum()
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &Input) -> i32 {
+    let mut frequency: i32 = 0;
+    let mut seen: HashSet<i32> = HashSet::new();
+    for x in input.iter().cycle() {
+        frequency += x;
+
+        if !seen.insert(frequency) {
+            return frequency;
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -35,7 +54,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 3)
     }
 
     #[test]
@@ -43,6 +62,6 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 2)
     }
 }
--- 2018/day02/src/main.rs
+++ 2018/day02/src/main.rs
@@ -1,48 +1,62 @@
-use nom::IResult;
+use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../input.txt");
-    let input = parse(input);
 
-    let answer = problem1(&input);
+    let answer = problem1(input);
     println!("problem 1 answer: {answer}");
 
-    let answer = problem2(&input);
+    let answer = problem2(input);
     println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
-
-fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
-
-    result.unwrap().1
+fn exactly(input: &str, count: usize) -> bool {
+    input
+        .chars()
+        .any(|c| input.chars().filter(|x| *x == c).count() == count)
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &str) -> usize {
+    let exactly_two = input.lines().filter(|s| exactly(s, 2)).count();
+    let exactly_three = input.lines().filter(|s| exactly(s, 3)).count();
+
+    exactly_two * exactly_three
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &str) -> String {
+    for x in input.lines() {
+        for y in input.lines() {
+            let xc: HashSet<(usize, char)> = x.chars().into_iter().enumerate().collect();
+            let yc: HashSet<(usize, char)> = y.chars().into_iter().enumerate().collect();
+            let diff: Vec<_> = xc.difference(&yc).into_iter().collect();
+
+            if diff.len() == 1 {
+                let index_to_remove = diff[0].0;
+                let (a, b) = x.split_at(index_to_remove);
+                let s = format!("{}{}", a, &b[1..]);
+
+                return s;
+            }
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{problem1, problem2};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
+        let result = problem1(input);
+        assert_eq!(result, 12)
     }
 
     #[test]
     fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let input = include_str!("../test2.txt");
+        let result = problem2(input);
+        assert_eq!(result, "fgij")
     }
 }
