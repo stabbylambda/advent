5fca9d4aeb7b96330a3543fee369fa03597c9a90
David Stone <david@stabbylambda.com>
2023-03-01 09:13:16 -0800

    Day 10

--- 2018/day10/src/main.rs
+++ 2018/day10/src/main.rs
@@ -1,48 +1,92 @@
-use nom::IResult;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{char, i32, newline, space0},
+    multi::separated_list1,
+    sequence::{delimited, preceded, separated_pair},
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
     let input = parse(input);
 
-    let answer = problem1(&input);
+    let answer = problem1(&input, 10);
     println!("problem 1 answer: {answer}");
-
-    let answer = problem2(&input);
-    println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<((i32, i32), (i32, i32))>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let angle_pair = |s| {
+        delimited(
+            char('<'),
+            separated_pair(preceded(space0, i32), tag(", "), preceded(space0, i32)),
+            char('>'),
+        )(s)
+    };
+    let result: IResult<&str, Input> = separated_list1(
+        newline,
+        separated_pair(
+            preceded(tag("position="), angle_pair),
+            char(' '),
+            preceded(tag("velocity="), angle_pair),
+        ),
+    )(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
-}
+fn problem1(input: &Input, expected_height: u32) -> i32 {
+    for t in 0.. {
+        let mut current: Vec<(i32, i32)> = vec![];
+        let mut min_x = i32::MAX;
+        let mut max_x = i32::MIN;
+        let mut min_y = i32::MAX;
+        let mut max_y = i32::MIN;
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+        for ((x, y), (vx, vy)) in input {
+            // get the points at time = t
+            let x = x + t * vx;
+            let y = y + t * vy;
+            current.push((x, y));
+
+            // keep track of the bounding box as we go
+            min_x = min_x.min(x);
+            max_x = max_x.max(x);
+            min_y = min_y.min(y);
+            max_y = max_y.max(y);
+        }
+
+        let height = min_y.abs_diff(max_y);
+
+        if height <= expected_height {
+            // I'm not writing some godawful OCR-like code to recognize ASCII art, just print the string, return time = t
+            for y in min_y..=max_y {
+                for x in min_x..=max_x {
+                    if current.contains(&(x, y)) {
+                        print!("#");
+                    } else {
+                        print!(" ");
+                    }
+                }
+                println!();
+            }
+
+            return t;
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, problem1};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
-    }
-
-    #[test]
-    fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let result = problem1(&input, 8);
+        assert_eq!(result, 3)
     }
 }
