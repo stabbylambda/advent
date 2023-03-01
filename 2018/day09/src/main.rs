66eaf2d1274314b689ca385e136ad3d8655760df
David Stone <david@stabbylambda.com>
2023-03-01 08:58:29 -0800

    Day 9

--- 2018/day09/src/main.rs
+++ 2018/day09/src/main.rs
@@ -1,48 +1,59 @@
-use nom::IResult;
+use std::collections::VecDeque;
 
 fn main() {
-    let input = include_str!("../input.txt");
-    let input = parse(input);
+    let input = (446, 71522);
 
-    let answer = problem1(&input);
+    let answer = problem1(input);
     println!("problem 1 answer: {answer}");
 
-    let answer = problem2(&input);
+    let answer = problem2(input);
     println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
+type Input = (usize, u32);
 
-fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+fn problem1((players, last_marble): Input) -> u32 {
+    let mut circle = VecDeque::new();
+    circle.push_back(0);
+    let mut scores = vec![0; players];
 
-    result.unwrap().1
+    for marble in 1..=last_marble {
+        let player = (marble - 1) as usize % players;
+
+        if marble % 23 == 0 {
+            circle.rotate_right(8);
+            let score = marble + circle.pop_front().unwrap();
+            scores[player] += score;
+            circle.rotate_left(1);
+        } else {
+            circle.rotate_left(1);
+            circle.push_back(marble);
+        }
+    }
+
+    *scores.iter().max().unwrap()
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
-}
-
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2((players, last_marble): Input) -> u32 {
+    problem1((players, last_marble * 100))
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::problem1;
     #[test]
     fn first() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
-    }
-
-    #[test]
-    fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let examples = [
+            ((9, 25), 32),
+            ((10, 1618), 8317),
+            ((13, 7999), 146373),
+            ((17, 1104), 2764),
+            ((21, 6111), 54718),
+            ((30, 5807), 37305),
+        ];
+        for (input, expected) in examples {
+            let result = problem1(input);
+            assert_eq!(result, expected);
+        }
     }
 }
