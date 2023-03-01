eb9277b8b2b3e37362a3ac0acfd3aa674ec2ffdc
David Stone <david@stabbylambda.com>
2023-02-28 20:25:41 -0800

    Day 8

--- 2018/day08/src/main.rs
+++ 2018/day08/src/main.rs
@@ -1,4 +1,8 @@
-use nom::IResult;
+use nom::{
+    character::complete::{char, u32},
+    multi::separated_list1,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -14,28 +18,88 @@ fn main() {
 type Input = Vec<u32>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = separated_list1(char(' '), u32)(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+#[derive(Debug)]
+struct Node {
+    children: Vec<Node>,
+    metadata: Vec<u32>,
+}
+impl Node {
+    fn sum_metadata(&self) -> u32 {
+        let self_metadata: u32 = self.metadata.iter().sum();
+        let child_metadata: u32 = self.children.iter().map(|x| x.sum_metadata()).sum();
+
+        self_metadata + child_metadata
+    }
+
+    fn child(&self, child_idx: &u32) -> u32 {
+        let child_idx = *child_idx as usize - 1;
+
+        self.children
+            .get(child_idx)
+            .map(|x| x.value())
+            .unwrap_or_default()
+    }
+
+    fn value(&self) -> u32 {
+        if self.children.is_empty() {
+            self.metadata.iter().sum()
+        } else {
+            self.metadata
+                .iter()
+                .map(|child_idx| self.child(child_idx))
+                .sum()
+        }
+    }
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn make_node(v: &[u32]) -> (usize, Node) {
+    let child_nodes = v[0];
+    let metadata_nodes = v[1];
+
+    let mut children = vec![];
+    let mut index = 2;
+    for _n in 0..child_nodes {
+        let (length, child) = make_node(&v[index..]);
+
+        index += length;
+        children.push(child);
+    }
+
+    let mut metadata = vec![];
+    for _n in 0..metadata_nodes {
+        metadata.push(v[index]);
+        index += 1;
+    }
+
+    (index, Node { children, metadata })
+}
+
+fn problem1(input: &Input) -> u32 {
+    let (len, node) = make_node(input);
+    assert!(len == input.len());
+    node.sum_metadata()
+}
+
+fn problem2(input: &Input) -> u32 {
+    let (len, node) = make_node(input);
+    assert!(len == input.len());
+    node.value()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{make_node, parse, problem1, problem2};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 138)
     }
 
     #[test]
@@ -43,6 +107,6 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 66)
     }
 }
