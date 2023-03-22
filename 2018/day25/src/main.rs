cfe623d96bbbced7c0fd79a911cd0e6b10602eae
David Stone <david@stabbylambda.com>
2023-03-22 15:52:04 -0700

    Day 25

--- 2018/day25/src/main.rs
+++ 2018/day25/src/main.rs
@@ -1,4 +1,12 @@
-use nom::IResult;
+use std::collections::BTreeSet;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{i32, newline},
+    combinator::map,
+    multi::separated_list1,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -6,43 +14,107 @@ fn main() {
 
     let answer = problem1(&input);
     println!("problem 1 answer: {answer}");
-
-    let answer = problem2(&input);
-    println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<Point>;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Point((i32, i32, i32, i32));
+
+impl Point {
+    fn manhattan(&self, other: &Point) -> u32 {
+        let (sx, sy, sz, sa) = self.0;
+        let (ox, oy, oz, oa) = other.0;
+
+        sx.abs_diff(ox) + sy.abs_diff(oy) + sz.abs_diff(oz) + sa.abs_diff(oa)
+    }
+}
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = separated_list1(
+        newline,
+        map(separated_list1(tag(","), i32), |x| {
+            Point((x[0], x[1], x[2], x[3]))
+        }),
+    )(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Constellation {
+    points: BTreeSet<Point>,
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+impl Constellation {
+    fn new(point: Point) -> Constellation {
+        let mut points = BTreeSet::new();
+        points.insert(point);
+
+        Constellation { points }
+    }
+
+    fn distance_from(&self, other: &Constellation) -> u32 {
+        let mut min_dist = u32::MAX;
+
+        for point in &self.points {
+            for other in &other.points {
+                min_dist = point.manhattan(other).min(min_dist);
+            }
+        }
+
+        min_dist
+    }
+
+    fn merge(&mut self, other: &Constellation) {
+        self.points.extend(other.points.clone())
+    }
+}
+
+fn problem1(input: &Input) -> usize {
+    let mut constellations: Vec<Constellation> =
+        input.iter().map(|x| Constellation::new(*x)).collect();
+
+    loop {
+        let merged: Vec<Constellation> = constellations.iter().fold(vec![], |mut result, c1| {
+            let can_merge = result
+                .iter_mut()
+                .find(|constellation| constellation.distance_from(c1) <= 3);
+
+            if let Some(constellation) = can_merge {
+                constellation.merge(c1);
+            } else {
+                result.push(c1.clone())
+            }
+
+            result
+        });
+
+        if merged.len() == constellations.len() {
+            break;
+        }
+
+        constellations = merged;
+    }
+
+    constellations.len()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, problem1};
     #[test]
     fn first() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
-    }
-
-    #[test]
-    fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let cases = [
+            (include_str!("../test1.txt"), 2),
+            (include_str!("../test2.txt"), 4),
+            (include_str!("../test3.txt"), 3),
+            (include_str!("../test4.txt"), 8),
+        ];
+        for (input, expected) in cases {
+            let input = parse(input);
+            let result = problem1(&input);
+            assert_eq!(result, expected)
+        }
     }
 }
