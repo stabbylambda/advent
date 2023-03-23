0555f88e9e0317cd331ad8a06f0bdccf44bd5343
David Stone <david@stabbylambda.com>
2023-03-23 10:23:36 -0700

    Day 1

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -1,4 +1,8 @@
-use nom::IResult;
+use nom::{
+    character::complete::{newline, u32},
+    multi::separated_list1,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -13,36 +17,55 @@ fn main() {
 
 type Input = Vec<u32>;
 
+fn fuel(mass: u32) -> u32 {
+    (mass / 3).saturating_sub(2)
+}
+
+fn recursive_fuel(mut mass: u32) -> u32 {
+    let mut total = 0;
+
+    loop {
+        mass = fuel(mass);
+        total += mass;
+
+        if mass == 0 {
+            break total;
+        }
+    }
+}
+
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = separated_list1(newline, u32)(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> u32 {
+    input.iter().map(|x| fuel(*x)).sum()
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &Input) -> u32 {
+    input.iter().map(|x| recursive_fuel(*x)).sum()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, problem1, recursive_fuel};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        let expected = 2 + 2 + 654 + 33583;
+        assert_eq!(result, expected)
     }
 
     #[test]
     fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let cases = [(14, 2), (1969, 966), (100756, 50346)];
+        for (input, expected) in cases {
+            let result = recursive_fuel(input);
+            assert_eq!(result, expected);
+        }
     }
 }
