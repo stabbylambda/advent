f14b0c36532fea9b004e8cf577c6a66e8d24865d
David Stone <david@stabbylambda.com>
2023-03-23 13:45:42 -0700

    Day 4

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -5,3 +5,22 @@ pub mod nom;
 pub mod orthogonal;
 pub mod program;
 pub mod union_find;
+
+pub fn digits(input: usize) -> Vec<u8> {
+    let mut input = input;
+    let mut v = vec![];
+
+    if input == 0 {
+        return vec![0];
+    }
+
+    while input != 0 {
+        let digit = input % 10;
+        input /= 10;
+
+        v.push(digit as u8);
+    }
+
+    v.reverse();
+    v
+}
