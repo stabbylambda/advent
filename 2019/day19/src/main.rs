7d77d87b55c62388d196f9aed91c3282b75e4b93
David Stone <david@stabbylambda.com>
2023-04-03 14:17:04 -0700

    Day 19

--- 2019/day19/src/main.rs
+++ 2019/day19/src/main.rs
@@ -13,12 +13,41 @@ fn main() {
 
 type Input = Intcode;
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+struct Robot<'a>(&'a Intcode);
+
+impl<'a> Robot<'a> {
+    fn pulled(&self, x: i64, y: i64) -> bool {
+        let mut program = self.0.clone();
+        program.input.push(y);
+        program.input.push(x);
+
+        program.execute();
+
+        program.get_last_output() == 1
+    }
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> i64 {
+    let robot = Robot(input);
+    let area = 50;
+    (0..area)
+        .map(|y| (0..area).map(|x| robot.pulled(x, y) as i64).sum::<i64>())
+        .sum()
+}
+
+fn problem2(input: &Input) -> i64 {
+    let robot = Robot(input);
+    let mut x = 0;
+    let mut y = 0;
+    while !robot.pulled(x + 99, y) {
+        y += 1;
+
+        while !robot.pulled(x, y + 99) {
+            x += 1;
+        }
+    }
+
+    x * 10_000 + y
 }
 
 #[cfg(test)]
@@ -28,17 +57,17 @@ mod test {
     use crate::{problem1, problem2};
     #[test]
     fn first() {
-        let input = include_str!("../test.txt");
+        let input = include_str!("../input.txt");
         let input = Intcode::parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 201)
     }
 
     #[test]
     fn second() {
-        let input = include_str!("../test.txt");
+        let input = include_str!("../input.txt");
         let input = Intcode::parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 6610984)
     }
 }
