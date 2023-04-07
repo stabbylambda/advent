020d884cb8f53d2118eac860240231fd6cc47af2
David Stone <david@stabbylambda.com>
2023-04-07 12:11:08 -0700

    Day 9

--- 2020/day09/src/main.rs
+++ 2020/day09/src/main.rs
@@ -1,4 +1,8 @@
-use nom::IResult;
+use nom::{
+    character::complete::{newline, u64},
+    multi::separated_list1,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -7,42 +11,77 @@ fn main() {
     let answer = problem1(&input);
     println!("problem 1 answer: {answer}");
 
-    let answer = problem2(&input);
+    let answer = problem2(&input, answer);
     println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<u64>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = separated_list1(newline, u64)(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn first_error(input: &[u64], preamble_size: usize) -> u64 {
+    // grab the digit after the preamble
+    if let Some(digit) = input.get(preamble_size + 1) {
+        let preamble = &input[0..=preamble_size];
+
+        // for each digit x in the preamble, check if digit - x  is in the preamble
+        let valid = preamble
+            .iter()
+            .filter_map(|x| digit.checked_sub(*x).filter(|y| x != y))
+            .any(|y| preamble.contains(&y));
+
+        return if valid {
+            first_error(&input[1..], preamble_size)
+        } else {
+            *digit
+        };
+    }
+
+    unreachable!("This code is valid?")
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> u64 {
+    first_error(input, 25)
+}
+
+fn problem2(input: &Input, goal: u64) -> u64 {
+    let idx = input.iter().position(|x| *x == goal).unwrap();
+
+    // this is the naive approach, start from the goal and work backwards, taking decreasing slices and summing them
+    // it's fast enough though
+    for n in (0..idx).rev() {
+        for size in 0..n {
+            let slice = &input[size..n];
+            let summed: u64 = slice.iter().sum();
+            if summed == goal {
+                return slice.iter().min().unwrap() + slice.iter().max().unwrap();
+            }
+        }
+    }
+
+    0
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{first_error, parse, problem2};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
-        let result = problem1(&input);
-        assert_eq!(result, 0)
+        let result = first_error(&input, 5);
+        assert_eq!(result, 127)
     }
 
     #[test]
     fn second() {
         let input = include_str!("../test.txt");
         let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let result = problem2(&input, 127);
+        assert_eq!(result, 62)
     }
 }
