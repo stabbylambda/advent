52c753b1ec9c50e88158fc4b5dd5f1be9529bd85
David Stone <david@stabbylambda.com>
2023-04-07 05:30:09 -0700

    Day 5

--- 2020/day05/src/main.rs
+++ 2020/day05/src/main.rs
@@ -1,4 +1,11 @@
-use nom::IResult;
+use nom::{
+    branch::alt,
+    character::complete::{char, newline},
+    combinator::map,
+    multi::{count, separated_list1},
+    sequence::tuple,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -11,38 +18,104 @@ fn main() {
     println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<Ticket>;
+
+enum Direction {
+    Front,
+    Back,
+    Left,
+    Right,
+}
+
+struct Ticket {
+    row: Vec<Direction>,
+    seat: Vec<Direction>,
+}
+
+impl Ticket {
+    fn row_number(&self) -> u32 {
+        self.split(127, &self.row)
+    }
+
+    fn seat_number(&self) -> u32 {
+        self.split(7, &self.seat)
+    }
+
+    fn seat_id(&self) -> u32 {
+        self.row_number() * 8 + self.seat_number()
+    }
+
+    fn split(&self, mut high: u32, dirs: &[Direction]) -> u32 {
+        let mut low: u32 = 0;
+        for dir in dirs {
+            let diff = (high.abs_diff(low) / 2) + 1;
+            match dir {
+                Direction::Front => high -= diff,
+                Direction::Back => low += diff,
+
+                Direction::Left => high -= diff,
+                Direction::Right => low += diff,
+            }
+        }
+
+        assert_eq!(low, high);
+
+        low
+    }
+}
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let direction = |s| {
+        alt((
+            map(char('F'), |_| Direction::Front),
+            map(char('B'), |_| Direction::Back),
+            map(char('L'), |_| Direction::Left),
+            map(char('R'), |_| Direction::Right),
+        ))(s)
+    };
+    let result: IResult<&str, Input> = separated_list1(
+        newline,
+        map(
+            tuple((count(direction, 7), count(direction, 3))),
+            |(row, seat)| Ticket { row, seat },
+        ),
+    )(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> u32 {
+    let seats: Vec<u32> = input.iter().map(|x| x.seat_id()).collect();
+    *seats.iter().max().unwrap()
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &Input) -> u32 {
+    let seats: Vec<u32> = input.iter().map(|x| x.seat_id()).collect();
+
+    let low = *seats.iter().min().unwrap();
+    let high = *seats.iter().max().unwrap();
+
+    (low..high)
+        .find(|n| !seats.contains(n) && seats.contains(&(n - 1)) && seats.contains(&(n + 1)))
+        .unwrap()
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, problem1};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 820)
     }
-
     #[test]
-    fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+    fn row_seat_test() {
+        let ticket = parse("FBFBBFFRLR");
+        let ticket = ticket.first().unwrap();
+
+        assert_eq!(ticket.row_number(), 44);
+        assert_eq!(ticket.seat_number(), 5);
     }
 }
