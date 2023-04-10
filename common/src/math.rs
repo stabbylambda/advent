c89829b4d1a083cd453d6f2e66ba78de1827ac8b
David Stone <david@stabbylambda.com>
2023-04-10 09:46:53 -0700

    Day 13

--- common/src/math.rs
+++ common/src/math.rs
@@ -25,3 +25,35 @@ pub const fn div_ceil(lhs: i64, rhs: i64) -> i64 {
         d
     }
 }
+
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+pub fn extended_gcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = extended_gcd(b % a, a);
+        (g, y - (b / a) * x, x)
+    }
+}
+
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Computing_multiplicative_inverses_in_modular_structures
+pub fn inverse(x: i64, n: i64) -> Option<i64> {
+    let (g, x, _) = extended_gcd(x, n);
+    (g == 1).then_some((x % n + n) % n)
+}
+
+// and finally https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+// this only works because everything is pairwise coprime
+pub fn chinese_remainder(residues: &[i64], modulii: &[i64]) -> Option<i64> {
+    let prod: i64 = modulii.iter().product();
+
+    let mut sum = 0;
+    let pairs = residues.iter().zip(modulii);
+
+    for (&residue, &modulus) in pairs {
+        let p = prod / modulus;
+        sum += residue * inverse(p, modulus)? * p
+    }
+
+    Some(sum % prod)
+}
