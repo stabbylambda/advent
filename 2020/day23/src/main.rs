4284f2ba05aa84763e48a31f51ce40a3b24c5497
David Stone <david@stabbylambda.com>
2023-04-19 14:30:57 -0700

    Day 23

--- 2020/day23/src/main.rs
+++ 2020/day23/src/main.rs
@@ -1,4 +1,6 @@
-use nom::IResult;
+use common::{digits, nom::usize, to_number};
+
+use nom::{combinator::map, IResult};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -11,20 +13,84 @@ fn main() {
     println!("problem 2 answer: {answer}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<usize>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = map(map(usize, digits), |x| {
+        x.iter().map(|x| *x as usize).collect()
+    })(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn successors(input: &[usize]) -> Vec<usize> {
+    let mut cups = vec![0; input.len() + 1];
+    for x in input.windows(2) {
+        cups[x[0]] = x[1];
+    }
+    cups[*input.last().unwrap()] = *input.first().unwrap();
+
+    cups
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn game(cups: &[usize], mut cup: usize, rounds: u32) -> Vec<usize> {
+    let mut cups = cups.to_vec();
+    let max = cups.len();
+    let wrapping_sub = |x: usize| match x.checked_sub(1) {
+        Some(x) if x > 0 => x,
+        _ => max - 1,
+    };
+
+    for _round in 0..rounds {
+        let a = cups[cup];
+        let b = cups[a];
+        let c = cups[b];
+
+        let mut dest = wrapping_sub(cup);
+        while dest == a || dest == b || dest == c {
+            dest = wrapping_sub(dest);
+        }
+
+        cups[cup] = cups[c];
+        let temp = cups[dest];
+        cups[dest] = a;
+        cups[c] = temp;
+        cup = cups[cup];
+    }
+
+    cups
+}
+
+fn problem1(input: &Input) -> u32 {
+    let cup = *input.first().unwrap();
+
+    let cups = successors(input);
+    let cups = game(&cups, cup, 100);
+
+    let mut answer = vec![];
+    let mut i = 1;
+    loop {
+        match cups[i] {
+            1 => break,
+            cup => {
+                answer.push(cup as u32);
+                i = cup;
+            }
+        }
+    }
+
+    to_number(&answer)
+}
+
+fn problem2(input: &Input) -> usize {
+    let cup = *input.first().unwrap();
+
+    let mut cups = input.to_vec();
+    cups.extend(10..=1_000_000);
+    let cups = successors(&cups);
+    let cups = game(&cups, cup, 10_000_000);
+
+    cups[1] * cups[cups[1]]
 }
 
 #[cfg(test)]
@@ -35,7 +101,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 67384529)
     }
 
     #[test]
@@ -43,6 +109,6 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 149_245_887_792)
     }
 }
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -8,6 +8,10 @@ pub mod orthogonal;
 pub mod program;
 pub mod union_find;
 
+pub fn to_number(value: &[u32]) -> u32 {
+    value.iter().fold(0, |acc, x| (acc * 10) + x)
+}
+
 pub fn digits(input: usize) -> Vec<u8> {
     let mut input = input;
     let mut v = vec![];
