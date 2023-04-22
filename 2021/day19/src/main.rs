8aa6ac0b0a7e132d73047c641b256617f5c6dbbb
David Stone <david@stabbylambda.com>
2023-04-21 17:03:01 -0700

    Day 19 but faster
    I got rid of the BTreeSet because that was just making everything slow.
    The flamegraph showed that most of the time was spent in allocation. I
    also got rid of the fingerprinting because that just turned out to be
    pretty useless.

--- 2021/day19/src/main.rs
+++ 2021/day19/src/main.rs
@@ -1,7 +1,9 @@
 use itertools::Itertools;
-use std::collections::{BTreeSet, VecDeque};
+use std::{
+    collections::{BTreeSet, VecDeque},
+    ops::{Add, Sub},
+};
 
-use nalgebra::MatrixXx3;
 use nom::{
     bytes::complete::tag,
     character::complete::{i32, newline},
@@ -15,11 +17,9 @@ fn main() {
     let input = include_str!("../input.txt");
     let input = parse(input);
 
-    let answer = problem1(&input);
-    println!("problem 1 answer: {answer}");
-
-    let answer = problem2(&input);
-    println!("problem 2 answer: {answer}");
+    let (answer1, answer2) = problem(&input);
+    println!("problem 1 answer: {answer1}");
+    println!("problem 2 answer: {answer2}");
 }
 
 type Input = Vec<Scanner>;
@@ -65,78 +65,78 @@ impl Point {
 
         x1.abs_diff(x2) + y1.abs_diff(y2) + z1.abs_diff(z2)
     }
+}
 
-    fn translation_vector(&self, other: &Self) -> (i32, i32, i32) {
+impl Sub for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Self) -> Self::Output {
         let Point(x1, y1, z1) = self;
-        let Point(x2, y2, z2) = *other;
+        let Point(x2, y2, z2) = rhs;
 
-        (x1 - x2, y1 - y2, z1 - z2)
+        Point(x1 - x2, y1 - y2, z1 - z2)
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let Point(x, y, z) = self;
+        let Point(ox, oy, oz) = rhs;
+        Point(x + ox, y + oy, z + oz)
     }
 }
 
 #[derive(Debug, Clone)]
 struct Scanner {
     id: i32,
-    beacons: BTreeSet<Point>,
-    fingerprint: BTreeSet<u32>,
+    beacons: Vec<Point>,
 }
 impl Scanner {
     fn new(id: i32, beacons: Vec<Point>) -> Self {
-        let beacons = beacons.into_iter().collect();
-        let fingerprint = Self::fingerprint(&beacons);
-        Scanner {
-            id,
-            beacons,
-            fingerprint,
-        }
-    }
-
-    // 12 choose 2 gets us to 66 so we can validate that a point cloud matches another point cloud
-    const MIN_MATCH: usize = 66;
-
-    fn fingerprint_match(&self, other: &Scanner) -> bool {
-        self.fingerprint.intersection(&other.fingerprint).count() >= Self::MIN_MATCH
-    }
-
-    fn fingerprint(beacons: &BTreeSet<Point>) -> BTreeSet<u32> {
-        // Get the manhattan distances between all pairs of two points
-        beacons
-            .iter()
-            .tuple_combinations()
-            .map(|(b1, b2)| b1.manhattan(b2))
-            .collect()
+        Scanner { id, beacons }
     }
 
     fn rotate(&self, rot: u8) -> Self {
         Self {
             id: self.id,
             beacons: self.beacons.iter().map(|x| x.rotate(rot)).collect(),
-            fingerprint: self.fingerprint.clone(),
         }
     }
 
-    fn translate(&self, (dx, dy, dz): (i32, i32, i32)) -> Self {
+    fn translate(&self, t: Point) -> Self {
         Self {
             id: self.id,
-            beacons: self
-                .beacons
-                .iter()
-                .map(|x| {
-                    let Point(x, y, z) = *x;
-                    Point(x + dx, y + dy, z + dz)
-                })
-                .collect(),
-            fingerprint: self.fingerprint.clone(),
+            beacons: self.beacons.iter().map(|x| *x + t).collect(),
         }
     }
 
     fn merge(&mut self, other: &Scanner) {
-        self.beacons.extend(other.beacons.iter().copied());
-        self.fingerprint = self
-            .fingerprint
-            .union(&other.fingerprint)
-            .copied()
-            .collect();
+        for beacon in &other.beacons {
+            if !self.beacons.contains(beacon) {
+                self.beacons.push(*beacon);
+            }
+        }
+    }
+
+    fn get_translated_scanner(&self, other: &Scanner) -> Option<(Scanner, Point)> {
+        self.beacons
+            .iter()
+            .cartesian_product(&other.beacons)
+            .find_map(|(p1, p2)| {
+                let offset = *p1 - *p2;
+
+                let other_beacons = other.beacons.iter().map(|p| *p + offset);
+
+                let num_matching = self
+                    .beacons
+                    .iter()
+                    .filter(|&point| other_beacons.clone().any(|p| p == *point))
+                    .count();
+
+                (num_matching >= 12).then(|| (other.translate(offset), offset))
+            })
     }
 }
 
@@ -159,92 +159,68 @@ fn parse(input: &str) -> Input {
     result.unwrap().1
 }
 
-fn merge_scanners(input: &Input) -> Scanner {
-    let mut scanners: VecDeque<Scanner> = input.clone().into_iter().collect();
+fn merge_scanners(input: &Input) -> (Scanner, Vec<Point>) {
+    let mut prime = input.first().unwrap().clone();
+    let mut coords = vec![Point(0, 0, 0)];
+
+    // precompute all the rotations
+    let mut scanners: VecDeque<Scanner> = input
+        .clone()
+        .into_iter()
+        .skip(1)
+        .flat_map(|x| (0..24).map(move |r| x.rotate(r)))
+        .collect();
+
     let mut merged: BTreeSet<i32> = BTreeSet::new();
 
-    while let Some(mut s1) = scanners.pop_front() {
+    while let Some(s1) = scanners.pop_front() {
+        // if we already merged one of these (because we precomputed all the rotations) we can just skip this one
         if merged.contains(&s1.id) {
             continue;
         }
 
-        // if this is the last scanner, we're done
-        if scanners.is_empty() {
-            return s1;
+        // get the translation vector
+        if let Some((s1, offset)) = prime.get_translated_scanner(&s1) {
+            println!("Merging {} into prime with offset {offset:?}", s1.id);
+
+            // merge all the stuff together
+            prime.merge(&s1);
+            merged.insert(s1.id);
+
+            // keep track of where the sensor is
+            coords.push(offset);
+        } else {
+            scanners.push_back(s1);
         }
-
-        println!(
-            "Checking sensor {}. Rest: {:?}",
-            s1.id,
-            scanners.iter().map(|x| x.id).collect::<Vec<i32>>()
-        );
-
-        let other_scanners: Vec<&Scanner> = scanners
-            .iter()
-            .filter(|s2| !merged.contains(&s2.id) && s1.id != s2.id && s1.fingerprint_match(s2))
-            .collect();
-
-        let reference_beacons = s1.beacons.clone();
-
-        'merge: for s2 in other_scanners {
-            println!("  Sensor {} neighbors sensor {}", s1.id, s2.id);
-            for rotation in 0..24 {
-                let s2r = s2.rotate(rotation);
-
-                let translation_vectors: BTreeSet<(i32, i32, i32)> = reference_beacons
-                    .iter()
-                    .cartesian_product(&s2r.beacons)
-                    .map(|(p1, p2)| p1.translation_vector(p2))
-                    .collect();
-
-                for v in translation_vectors {
-                    let s2t = s2r.translate(v);
-                    if s2t.beacons.intersection(&s1.beacons).count() >= 12 {
-                        println!("   Merged {} into {}", s2t.id, s1.id);
-                        s1.merge(&s2t);
-                        merged.insert(s2t.id);
-                        continue 'merge;
-                    }
-                }
-            }
-        }
-
-        println!("Resuling scanner has {} beacons", s1.beacons.len());
-
-        // now that we've merged into this one, push it back on
-        scanners.push_back(s1);
     }
 
-    unreachable!()
+    (prime, coords)
 }
 
-fn problem1(input: &Input) -> usize {
-    let result = merge_scanners(input);
+fn problem(input: &Input) -> (usize, u32) {
+    let (result, scanner_coords) = merge_scanners(input);
 
-    result.beacons.len()
-}
+    let answer1 = result.beacons.len();
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+    let answer2 = scanner_coords
+        .iter()
+        .tuple_combinations()
+        .map(|(p1, p2)| p1.manhattan(p2))
+        .max()
+        .unwrap();
+
+    (answer1, answer2)
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, problem};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
-        let result = problem1(&input);
-        assert_eq!(result, 79)
-    }
-
-    #[test]
-    #[ignore]
-    fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        let (result1, result2) = problem(&input);
+        assert_eq!(result1, 79);
+        assert_eq!(result2, 3621);
     }
 }
