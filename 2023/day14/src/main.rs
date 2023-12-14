921b3f09c40e8de7a6ec1b83472c3ec49902eb15
David Stone <david@stabbylambda.com>
2023-12-14 00:46:09 -0800

    Day 14

--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
 use std::{fmt::Debug, usize};
+
+use std::fmt::Display;
 
 use nom::{
     branch::alt,
@@ -38,9 +41,10 @@ fn parse(input: &str) -> Input {
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+/** Represents the tiles on the platform. Ordering is important because "tilting" is based on sort order */
 enum Tile {
-    RoundedRock,
     Empty,
+    RoundedRock,
     CubeRock,
 }
 
@@ -66,60 +70,142 @@ impl Platform {
         let height = v.len();
         let width = v[0].len();
 
-        Self {
+        let mut s = Self {
             platform: v,
             height,
             width,
-        }
-    }
-    fn get_column(&self, i: usize) -> Vec<Tile> {
-        self.platform.iter().map(|row| row[i]).collect()
+        };
+
+        // rotate so that the end of each row is "north"
+        s.rotate();
+        s
     }
 
     fn load(&self) -> usize {
-        (0..self.width)
-            .map(|x| {
-                self.get_column(x)
-                    .iter()
+        self.platform
+            .iter()
+            .map(|row| {
+                row.iter()
                     .enumerate()
-                    .filter_map(|(row_num, tile)| {
-                        (*tile == Tile::RoundedRock).then_some(self.height - row_num)
-                    })
+                    .filter_map(|(weight, tile)| (*tile == Tile::RoundedRock).then_some(weight + 1))
                     .sum::<usize>()
             })
-            .sum::<usize>()
+            .sum()
     }
 
-    fn tilt_north(&mut self) {
-        for i in 0..self.width {
-            let col = self.get_column(i);
-            let sorted = col
-                // split at the rocks, inclusive
-                .split_inclusive(|x| *x == Tile::CubeRock)
-                .flat_map(|s| {
-                    // sort them, because of the way the sort is defined, we'll get all the boulders first, then empty, then the cube rock
-                    let mut s1 = s.to_vec().clone();
-                    s1.sort();
-                    s1
-                })
-                .collect::<Vec<Tile>>();
-
-            // rearrange everything
-            for (row_idx, new_tile) in sorted.iter().enumerate() {
-                self.platform[row_idx][i] = *new_tile;
+    fn rotate(&mut self) {
+        let mut platform = self.platform.clone();
+        platform.reverse();
+        for i in 0..platform.len() {
+            for j in i..platform.len() {
+                // I'm sure we can use mem::swap here, but that probably involves slicing and isn't as clear
+                let x = platform[i][j];
+                let y = platform[j][i];
+                platform[j][i] = x;
+                platform[i][j] = y;
             }
         }
+        self.width = platform[0].len();
+        self.height = platform.len();
+        self.platform = platform;
+    }
+
+    fn spin_cycle(&mut self) {
+        // go through an entire cycle
+        for _dir in 0..4 {
+            self.tilt();
+            self.rotate();
+        }
+    }
+
+    /** Tilt the platform. We always orient so that the direction we're tilting is row-wise to the end. That way we don't
+     * have to deal with stupid columnar math. Should also make Vec operations faster since we have better memory-locality?
+     */
+    fn tilt(&mut self) {
+        let sorted: Vec<Vec<Tile>> = self
+            .platform
+            .iter()
+            .map(|slice| {
+                slice
+                    // split at the cube rocks, inclusive, so we get slices that end in a rock
+                    .split_inclusive(|x| *x == Tile::CubeRock)
+                    .flat_map(|s| {
+                        let mut s1 = s.to_vec().clone();
+                        // sort them in place, this will get all the empty spaces, then the boulders, then the cube rocks
+                        // which is neat because then we basically have "falling" for free
+                        s1.sort();
+                        s1
+                    })
+                    .collect::<Vec<Tile>>()
+            })
+            .collect();
+
+        self.platform = sorted;
+    }
+}
+
+impl Display for Platform {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let p = self;
+
+        for x in 0..p.width {
+            for y in 0..p.height {
+                write!(f, "{:?}", p.platform[x][y])?;
+            }
+
+            writeln!(f)?;
+        }
+
+        writeln!(f)?;
+
+        Ok(())
     }
 }
 
 fn problem1(input: &Input) -> usize {
     let mut platform: Platform = input.clone();
-    platform.tilt_north();
+    platform.tilt();
     platform.load()
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &Input) -> usize {
+    let mut platform: Platform = input.clone();
+    // keep a cache of the boards and the last cycle we saw that configuration
+    let mut platform_cache: HashMap<String, usize> = HashMap::new();
+
+    let limit = 1_000_000_000;
+
+    // start on cycle 1
+    let mut current = 1;
+    let mut load = 0;
+
+    while current < limit {
+        // do the spin cycle and get the load
+        platform.spin_cycle();
+        load = platform.load();
+
+        // insert the current state of the board and get the last time we saw it
+        if let Some(previous) = platform_cache.insert(platform.to_string(), current) {
+            // if we've seen it before, then we need to do some cool cycle math from 2022 day 17
+            let cycle_size = previous.abs_diff(current);
+            let skip_count = (limit - current) / cycle_size;
+            let skipped = skip_count * cycle_size;
+
+            println!(
+                "Found cycle at {current}. Skipping {skip_count} cycles of {cycle_size} size for {skipped}"
+            );
+
+            // skip that count
+            current += skipped;
+
+            // don't let it find a new cycle on the next line, just blow away everything
+            platform_cache.clear();
+        } else {
+            current += 1;
+        };
+    }
+
+    load
 }
 
 #[cfg(test)]
@@ -145,6 +231,6 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 64)
     }
 }
