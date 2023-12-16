9f0e94d716c06c18453c818d4122f7367edc9125
David Stone <david@stabbylambda.com>
2023-12-15 23:28:23 -0800

    Day 16 part 1

--- 2023/day16/src/main.rs
+++ 2023/day16/src/main.rs
@@ -1,4 +1,13 @@
-use nom::IResult;
+use std::collections::{BTreeSet, VecDeque};
+
+use common::map::{Map, MapSquare};
+use nom::{
+    branch::alt,
+    character::complete::{char, newline},
+    combinator::map,
+    multi::{many1, separated_list1},
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -11,16 +20,103 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
+type Input = Map<Tile>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = map(
+        separated_list1(
+            newline,
+            many1(alt((
+                map(char('.'), |_| Tile::Empty),
+                map(char('/'), |_| Tile::ForwardMirror),
+                map(char('\\'), |_| Tile::BackwardMirror),
+                map(char('|'), |_| Tile::VerticalSplitter),
+                map(char('-'), |_| Tile::HorizontalSplitter),
+            ))),
+        ),
+        Map::new,
+    )(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+#[derive(Clone, Copy, Debug)]
+enum Tile {
+    Empty,
+    VerticalSplitter,
+    HorizontalSplitter,
+    ForwardMirror,
+    BackwardMirror,
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+fn problem1(input: &Input) -> usize {
+    use Direction::*;
+    use Tile::*;
+    let start = ((0, 0), Direction::East);
+    let mut queue: VecDeque<((usize, usize), Direction)> = VecDeque::new();
+    queue.push_back(start);
+
+    let mut visited: BTreeSet<((usize, usize), Direction)> = BTreeSet::new();
+
+    while let Some(data @ (coords, direction)) = queue.pop_front() {
+        let tile = input.get(coords);
+        if !visited.insert(data) {
+            continue;
+        };
+
+        let new_tiles = match (tile.data, direction) {
+            (Empty, North)
+            | (VerticalSplitter, North)
+            | (ForwardMirror, East)
+            | (BackwardMirror, West) => {
+                vec![tile.neighbors().north.map(|n| (n.coords, North))]
+            }
+            (Empty, South)
+            | (VerticalSplitter, South)
+            | (ForwardMirror, West)
+            | (BackwardMirror, East) => {
+                vec![tile.neighbors().south.map(|n| (n.coords, South))]
+            }
+            (Empty, East)
+            | (HorizontalSplitter, East)
+            | (ForwardMirror, North)
+            | (BackwardMirror, South) => {
+                vec![tile.neighbors().east.map(|n| (n.coords, East))]
+            }
+            (Empty, West)
+            | (HorizontalSplitter, West)
+            | (ForwardMirror, South)
+            | (BackwardMirror, North) => {
+                vec![tile.neighbors().west.map(|n| (n.coords, West))]
+            }
+            (VerticalSplitter, East) | (VerticalSplitter, West) => {
+                vec![
+                    tile.neighbors().north.map(|n| (n.coords, North)),
+                    tile.neighbors().south.map(|n| (n.coords, South)),
+                ]
+            }
+            (HorizontalSplitter, North) | (HorizontalSplitter, South) => {
+                vec![
+                    tile.neighbors().east.map(|n| (n.coords, East)),
+                    tile.neighbors().west.map(|n| (n.coords, West)),
+                ]
+            }
+        };
+
+        for t in new_tiles.into_iter().flatten() {
+            queue.push_back(t);
+        }
+    }
+
+    let coords_only: BTreeSet<(usize, usize)> = visited.into_iter().map(|(c, _dir)| c).collect();
+    coords_only.len()
 }
 
 fn problem2(_input: &Input) -> u32 {
@@ -35,7 +131,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 46)
     }
 
     #[test]
