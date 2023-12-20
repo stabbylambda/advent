0cfaba7415cd37d916e1b05a44ff5d25f7ca9ae0
David Stone <david@stabbylambda.com>
2023-12-19 23:03:29 -0800

    Day 20 part 1

---
+++ 2023/day20/src/conjunction.rs
@@ -0,0 +1,40 @@
+use std::collections::BTreeMap;
+
+use crate::Pulse;
+
+#[derive(Debug)]
+pub struct Conjunction {
+    last_pulses: BTreeMap<String, Pulse>,
+}
+
+impl<'a> Conjunction {
+    pub fn new(incoming: &[&str]) -> Self {
+        Self {
+            last_pulses: incoming
+                .iter()
+                .map(|name| (name.to_string(), Pulse::Low))
+                .collect(),
+        }
+    }
+    pub fn receive(&mut self, from: &'a str, pulse: Pulse) -> Option<Pulse> {
+        self.last_pulses.insert(from.to_string(), pulse);
+
+        if self.last_pulses.values().all(|p| *p == Pulse::High) {
+            Some(Pulse::Low)
+        } else {
+            Some(Pulse::High)
+        }
+    }
+}
+
+#[test]
+fn conjunction_test() {
+    let mut c = Conjunction::new(&["foo", "bar", "baz"]);
+
+    assert_eq!(c.receive("foo", Pulse::High), Some(Pulse::High));
+    assert_eq!(c.receive("bar", Pulse::High), Some(Pulse::High));
+    assert_eq!(c.receive("baz", Pulse::High), Some(Pulse::Low));
+    assert_eq!(c.receive("baz", Pulse::Low), Some(Pulse::High));
+    assert_eq!(c.receive("baz", Pulse::High), Some(Pulse::Low));
+    assert_eq!(c.receive("bar", Pulse::High), Some(Pulse::Low));
+}
--- 2023/day20/src/main.rs
+++ 2023/day20/src/main.rs
@@ -1,4 +1,19 @@
-use nom::IResult;
+use std::collections::{BTreeMap, VecDeque};
+
+use advent_2023_20::broadcaster::Broadcaster;
+use advent_2023_20::conjunction::Conjunction;
+use advent_2023_20::flip_flop::FlipFlop;
+use advent_2023_20::{ModuleKind, Pulse};
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, newline},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -11,16 +26,128 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
+type Input<'a> = BTreeMap<&'a str, (ModuleIdentifier, Vec<&'a str>)>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = map(
+        separated_list1(
+            newline,
+            separated_pair(
+                alt((
+                    map(preceded(tag("%"), alpha1), |x| {
+                        (x, ModuleIdentifier::FlipFlop)
+                    }),
+                    map(preceded(tag("&"), alpha1), |x| {
+                        (x, ModuleIdentifier::Conjunction)
+                    }),
+                    map(tag("broadcaster"), |x| (x, ModuleIdentifier::Broadcaster)),
+                )),
+                tag(" -> "),
+                separated_list1(tag(", "), alpha1),
+            ),
+        ),
+        |x| {
+            x.into_iter()
+                .map(|((sender, kind), receivers)| (sender, (kind, receivers)))
+                .collect()
+        },
+    )(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+#[derive(Debug, PartialEq, Eq)]
+enum ModuleIdentifier {
+    FlipFlop,
+    Conjunction,
+    Broadcaster,
+}
+
+fn problem1(input: &Input) -> u32 {
+    let mut modules = create_modules(input);
+
+    let mut high_pulses = 0;
+    let mut low_pulses = 0;
+
+    for _i in 0..1000 {
+        let mut queue: VecDeque<(&str, Pulse, &str)> = VecDeque::new();
+        queue.push_back(("button", Pulse::Low, "broadcaster"));
+
+        while let Some((from, pulse, target)) = queue.pop_front() {
+            match pulse {
+                Pulse::High => high_pulses += 1,
+                Pulse::Low => low_pulses += 1,
+            };
+
+            if let Some((module, targets)) = modules.get_mut(target) {
+                if let Some(result) = module.receive(from, pulse) {
+                    for t in targets {
+                        queue.push_back((target, result, t))
+                    }
+                }
+            }
+        }
+    }
+
+    high_pulses * low_pulses
+}
+
+type ModuleMap<'a> = BTreeMap<&'a str, (ModuleKind, Vec<&'a str>)>;
+fn create_modules<'a>(
+    input: &BTreeMap<&'a str, (ModuleIdentifier, Vec<&'a str>)>,
+) -> ModuleMap<'a> {
+    let mut modules: ModuleMap = BTreeMap::new();
+
+    // map all the incoming modules for conjunctions
+    let conjunctions: Vec<(&&str, &Vec<&str>)> = input
+        .iter()
+        .filter_map(|(name, (k, targets))| {
+            (*k == ModuleIdentifier::Conjunction).then_some((name, targets))
+        })
+        .collect();
+
+    for (name, targets) in conjunctions {
+        let incoming: Vec<&str> = input
+            .iter()
+            .filter_map(|(incoming_name, (_k, targets))| {
+                targets.contains(name).then_some(*incoming_name)
+            })
+            .collect();
+
+        modules.insert(
+            *name,
+            (
+                ModuleKind::Conjunction(Conjunction::new(&incoming)),
+                targets.clone(),
+            ),
+        );
+    }
+
+    // create the broadcaster
+    modules.insert(
+        "broadcaster",
+        (
+            ModuleKind::Broadcaster(Broadcaster::new()),
+            input["broadcaster"].1.clone(),
+        ),
+    );
+
+    // create the flipflops
+    let flip_flops: Vec<_> = input
+        .iter()
+        .filter_map(|(name, (k, targets))| {
+            (*k == ModuleIdentifier::FlipFlop).then_some((
+                *name,
+                (ModuleKind::FlipFlop(FlipFlop::new()), targets.clone()),
+            ))
+        })
+        .collect();
+
+    for (name, (module, targets)) in flip_flops {
+        modules.insert(name, (module, targets));
+    }
+
+    modules
 }
 
 fn problem2(_input: &Input) -> u32 {
@@ -35,7 +162,14 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 32000000)
+    }
+    #[test]
+    fn first_interesting() {
+        let input = include_str!("../test1_interesting.txt");
+        let input = parse(input);
+        let result = problem1(&input);
+        assert_eq!(result, 11687500)
     }
 
     #[test]
