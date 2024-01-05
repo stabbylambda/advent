0ee0b0ca8551e826fdcc80ba079927b09227424e
David Stone <david@stabbylambda.com>
2024-01-04 23:57:58 -0800

    Cleanup

--- 2021/day23/src/main.rs
+++ 2021/day23/src/main.rs
@@ -1,7 +1,6 @@
 use itertools::Itertools;
 use std::{
     collections::{BinaryHeap, HashMap},
-    fmt::Display,
     usize, vec,
 };
 
@@ -17,13 +16,13 @@ fn main() {
     let input = include_str!("../input.txt");
     let input = parse(input);
 
-    let answer = problem1(&input);
+    let answer = problem(&input);
     println!("problem 1 answer: {answer}");
 
     let input = include_str!("../input2.txt");
     let input = parse(input);
 
-    let answer = problem2(&input);
+    let answer = problem(&input);
     println!("problem 2 answer: {answer}");
 }
 
@@ -147,6 +146,16 @@ impl Amphipods {
         (0..4).all(|x| self.is_room_done(x))
     }
 
+    fn is_hallway_clear(&self, from: usize, to: usize) -> bool {
+        let (min_idx, max_idx) = if from < to {
+            (from + 1, to)
+        } else {
+            (to, from - 1)
+        };
+
+        (min_idx..max_idx).all(|x| self.hallway[x].is_none())
+    }
+
     fn open_hallway_locations(&self, room: &Room) -> Vec<usize> {
         let hallway_index = room.matching_hallway_index();
 
@@ -161,13 +170,6 @@ impl Amphipods {
         left.chain(right).collect_vec()
     }
 }
-
-const HALLWAY_COSTS: [[usize; 11]; 4] = [
-    [3, 2, 0, 2, 0, 4, 0, 6, 0, 8, 9], // room A
-    [5, 4, 0, 2, 0, 2, 0, 4, 0, 6, 7], // room B
-    [7, 6, 0, 4, 0, 2, 0, 2, 0, 4, 5], // room C
-    [9, 8, 0, 6, 0, 4, 0, 2, 0, 2, 3], // room D
-];
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct State {
@@ -190,32 +192,23 @@ impl State {
             .iter()
             .enumerate()
             .filter_map(|(hallway_idx, a)| a.map(|amphipod| (hallway_idx, amphipod)))
-            .filter_map(|(hallway_idx, amphipod)| {
+            .filter_map(|(from, amphipod)| {
                 let room_idx = amphipod as usize;
                 let room = &self.amphipods.rooms[room_idx];
+                let to = room.matching_hallway_index();
 
                 if !room.is_open_to(amphipod) {
                     return None;
                 }
 
-                let target_hallway_idx = room.matching_hallway_index();
-
-                let (min_idx, max_idx) = if hallway_idx < target_hallway_idx {
-                    (hallway_idx + 1, target_hallway_idx)
-                } else {
-                    (target_hallway_idx, hallway_idx - 1)
-                };
-
-                let clear = (min_idx..max_idx).all(|x| self.amphipods.hallway[x].is_none());
-
-                if !clear {
+                if !self.amphipods.is_hallway_clear(from, to) {
                     return None;
                 }
 
-                let depth = room.1.iter().rposition(|x| x.is_none()).unwrap();
-                let steps = HALLWAY_COSTS[room_idx][hallway_idx] + depth;
-
-                Some(self.swap_places(hallway_idx, room_idx, depth, amphipod.energy(steps)))
+                room.1
+                    .iter()
+                    .rposition(|x| x.is_none())
+                    .and_then(|depth| self.swap_places(amphipod, from, room_idx, depth))
             })
             .collect()
     }
@@ -228,17 +221,13 @@ impl State {
             .filter(|room| room.has_mismatched_amphipods())
             .flat_map(|room| {
                 let room_idx = room.0;
-                let (a, depth) = room.topmost_amphipod();
+                let (amphipod, depth) = room.topmost_amphipod();
 
                 self.amphipods
                     .open_hallway_locations(room)
                     .into_iter()
                     .filter_map(move |hallway_idx| {
-                        let steps = HALLWAY_COSTS[room_idx][hallway_idx];
-                        (steps > 0).then_some((hallway_idx, depth + steps))
-                    })
-                    .map(move |(hallway_index, steps)| {
-                        self.swap_places(hallway_index, room_idx, depth, a.energy(steps))
+                        self.swap_places(amphipod, hallway_idx, room_idx, depth)
                     })
             })
             .collect::<Vec<_>>()
@@ -246,11 +235,18 @@ impl State {
 
     fn swap_places(
         &self,
+        amphipod: Amphipod,
         hallway_index: usize,
         room_index: usize,
         depth: usize,
-        energy: usize,
-    ) -> Self {
+    ) -> Option<Self> {
+        let steps = HALLWAY_COSTS[room_index][hallway_index];
+        if steps == 0 {
+            return None;
+        }
+
+        let energy = amphipod.energy(depth + steps);
+
         // swap the hallway spot with the room spot
         let mut amphipods = self.amphipods.clone();
         std::mem::swap(
@@ -258,12 +254,19 @@ impl State {
             &mut amphipods.rooms[room_index].1[depth],
         );
 
-        State {
+        Some(State {
             amphipods,
             energy_spent: self.energy_spent + energy,
-        }
+        })
     }
 }
+
+static HALLWAY_COSTS: [[usize; 11]; 4] = [
+    [3, 2, 0, 2, 0, 4, 0, 6, 0, 8, 9], // room A
+    [5, 4, 0, 2, 0, 2, 0, 4, 0, 6, 7], // room B
+    [7, 6, 0, 4, 0, 2, 0, 2, 0, 4, 5], // room C
+    [9, 8, 0, 6, 0, 4, 0, 2, 0, 2, 3], // room D
+];
 
 impl PartialOrd for State {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
@@ -277,7 +280,7 @@ impl Ord for State {
     }
 }
 
-fn problem1(input: &Input) -> usize {
+fn problem(input: &Input) -> usize {
     let mut queue = BinaryHeap::new();
     queue.push(State {
         amphipods: input.clone(),
@@ -303,78 +306,16 @@ fn problem1(input: &Input) -> usize {
     }
 
     0
-}
-
-fn problem2(input: &Input) -> usize {
-    let mut queue = BinaryHeap::new();
-    queue.push(State {
-        amphipods: input.clone(),
-        energy_spent: 0,
-    });
-
-    let mut cost_cache = HashMap::new();
-    cost_cache.insert(input.clone(), 0);
-
-    while let Some(state) = queue.pop() {
-        if state.amphipods.is_done() {
-            return state.energy_spent;
-        }
-
-        let t = state.all_transitions();
-
-        for next in t {
-            if next.energy_spent < *cost_cache.get(&next.amphipods).unwrap_or(&usize::MAX) {
-                cost_cache.insert(next.amphipods.clone(), next.energy_spent);
-                queue.push(next);
-            }
-        }
-    }
-
-    0
-}
-
-impl Display for Amphipods {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        fn amphipod(a: Option<Amphipod>) -> String {
-            match a {
-                Some(Amphipod::Amber) => "A".to_string(),
-                Some(Amphipod::Bronze) => "B".to_string(),
-                Some(Amphipod::Copper) => "C".to_string(),
-                Some(Amphipod::Desert) => "D".to_string(),
-                None => ".".to_string(),
-            }
-        }
-
-        let top: Vec<String> = self.rooms.iter().map(|x| amphipod(x.1[0])).collect();
-        let bottom: Vec<String> = self.rooms.iter().map(|x| amphipod(x.1[1])).collect();
-
-        writeln!(f, "#############")?;
-        write!(f, "#")?;
-        for x in self.hallway {
-            write!(f, "{}", amphipod(x))?;
-        }
-        writeln!(f, "#")?;
-
-        writeln!(f, "###{}#{}#{}#{}###", top[0], top[1], top[2], top[3])?;
-        writeln!(
-            f,
-            "  #{}#{}#{}#{}#  ",
-            bottom[0], bottom[1], bottom[2], bottom[3]
-        )?;
-        writeln!(f, "  #########  ")?;
-
-        Ok(())
-    }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, problem};
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
-        let result = problem1(&input);
+        let result = problem(&input);
         assert_eq!(result, 12521)
     }
 
@@ -382,7 +323,7 @@ mod test {
     fn second() {
         let input = include_str!("../test2.txt");
         let input = parse(input);
-        let result = problem2(&input);
+        let result = problem(&input);
         assert_eq!(result, 44169)
     }
 }
