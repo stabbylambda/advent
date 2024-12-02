2a2e48c40a3ce0a8862993760956bcdfcaef157b
David Stone <david@stabbylambda.com>
2024-12-01 22:19:38 -0800

    2024 day 2

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -1,8 +1,14 @@
-use nom::IResult;
+use itertools::Itertools;
+use nom::{
+    character::complete::{char, i32, newline},
+    combinator::map,
+    multi::separated_list1,
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
-    let input = parse(&input);
+    let input = parse(input);
 
     let score = problem1(&input);
     println!("problem 1 score: {score}");
@@ -11,20 +17,58 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
-
+type Input = Vec<Report>;
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> =
+        separated_list1(newline, map(separated_list1(char(' '), i32), Report))(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+struct Report(Vec<i32>);
+
+impl Report {
+    fn is_safe(&self) -> bool {
+        let deltas = self
+            .0
+            .iter()
+            .tuple_windows()
+            .map(|(a, b)| a - b)
+            .collect_vec();
+
+        let monotonic = deltas.iter().all(|x| *x > 0) || deltas.iter().all(|x| *x < 0);
+        let in_range = deltas.iter().all(|x| x.abs() <= 3);
+
+        monotonic && in_range
+    }
+
+    fn without_index(&self, i: usize) -> Self {
+        // make a clone and drop the index
+        let mut v = self.0.clone();
+        v.remove(i);
+        Self(v)
+    }
+
+    fn is_safe_with_dampener(&self) -> bool {
+        // if we're already safe, just bail
+        if self.is_safe() {
+            return true;
+        }
+
+        // otherwise remove each element and find out if we're safe without it (there's probably some more optimal
+        // way, but who cares)
+        (0..self.0.len())
+            .map(|i| self.without_index(i))
+            .any(|r| r.is_safe())
+    }
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> usize {
+    input.iter().filter(|x| x.is_safe()).count()
+}
+
+fn problem2(input: &Input) -> usize {
+    input.iter().filter(|x| x.is_safe_with_dampener()).count()
 }
 
 #[cfg(test)]
@@ -33,16 +77,16 @@ mod test {
     #[test]
     fn first() {
         let input = include_str!("../test.txt");
-        let input = parse(&input);
+        let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 2)
     }
 
     #[test]
     fn second() {
         let input = include_str!("../test.txt");
-        let input = parse(&input);
+        let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 4)
     }
 }
