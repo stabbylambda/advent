8a75935aa66194c2e44ee29bd79590e206839125
David Stone <david@stabbylambda.com>
2024-12-02 23:28:05 -0800

    2024 day 3

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -1,8 +1,17 @@
-use nom::IResult;
+use common::nom::drop_till;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::i32,
+    combinator::map,
+    multi::many1,
+    sequence::{delimited, separated_pair},
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
-    let input = parse(&input);
+    let input = parse(input);
 
     let score = problem1(&input);
     println!("problem 1 score: {score}");
@@ -11,20 +20,46 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
+#[derive(Debug)]
+enum Instruction {
+    Mul(i32, i32),
+    Enable,
+    Disable,
+}
+
+type Input = Vec<Instruction>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = many1(drop_till(alt((
+        map(
+            delimited(tag("mul("), separated_pair(i32, tag(","), i32), tag(")")),
+            |(a, b)| Instruction::Mul(a, b),
+        ),
+        map(tag("do()"), |_| Instruction::Enable),
+        map(tag("don't()"), |_| Instruction::Disable),
+    ))))(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> i32 {
+    input
+        .iter()
+        .map(|x| match *x {
+            Instruction::Mul(a, b) => a * b,
+            _ => 0,
+        })
+        .sum()
 }
 
-fn problem2(_input: &Input) -> u32 {
-    todo!()
+fn problem2(input: &Input) -> i32 {
+    let (_, result) = input.iter().fold((true, 0), |(enabled, acc), x| match *x {
+        Instruction::Mul(a, b) if enabled => (enabled, acc + a * b),
+        Instruction::Enable => (true, acc),
+        Instruction::Disable => (false, acc),
+        _ => (enabled, acc),
+    });
+    result
 }
 
 #[cfg(test)]
@@ -32,17 +67,17 @@ mod test {
     use crate::{parse, problem1, problem2};
     #[test]
     fn first() {
-        let input = include_str!("../test.txt");
-        let input = parse(&input);
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 161)
     }
 
     #[test]
     fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(&input);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 48)
     }
 }
