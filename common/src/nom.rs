17645189189c9f585767ac0bc6ce7d4b5b7786fe
David Stone <david@stabbylambda.com>
2024-12-02 23:28:05 -0800

    2024 day 3

--- common/src/nom.rs
+++ common/src/nom.rs
@@ -1,12 +1,22 @@
 use nom::{
-    bytes::complete::tag,
+    bytes::complete::{tag, take},
     character::complete::{anychar, u32},
     combinator::{map, map_opt},
+    error::ParseError,
+    multi::many_till,
     sequence::separated_pair,
-    IResult,
+    IResult, InputIter, InputLength, InputTake, Parser,
 };
 
 use crate::map::Coord;
+
+pub fn drop_till<I, O, E: ParseError<I>, F>(parser: F) -> impl FnMut(I) -> IResult<I, O, E>
+where
+    I: Clone + InputLength + InputTake + InputIter,
+    F: Parser<I, O, E>,
+{
+    map(many_till(take(1u8), parser), |(_, matched)| matched)
+}
 
 pub fn single_digit(s: &str) -> IResult<&str, u32> {
     map_opt(anychar, |c| c.to_digit(10))(s)
