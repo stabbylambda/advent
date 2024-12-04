666fca52ce9b9a53f70a3ef63439fcd6bddbfe1e
David Stone <david@stabbylambda.com>
2024-12-03 22:55:08 -0800

    2024 day 4 part 1

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -1,4 +1,11 @@
-use nom::IResult;
+use common::map::{Map, MapSquare};
+use nom::{
+    branch::alt,
+    character::complete::{char, newline},
+    combinator::map,
+    multi::{many1, separated_list1},
+    IResult,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -11,16 +18,54 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<u32>;
+type Input = Map<char>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
-
+    let result: IResult<&str, Input> = map(
+        separated_list1(
+            newline,
+            many1(alt((char('X'), char('M'), char('A'), char('S')))),
+        ),
+        Map::new,
+    )(input);
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
-    todo!()
+fn problem1(input: &Input) -> usize {
+    input.into_iter().map(|x| spells(&x, "XMAS", None)).sum()
+}
+
+fn spells(x: &MapSquare<'_, char>, s: &str, dir: Option<usize>) -> usize {
+    match s.chars().next() {
+        Some(c) if &c != x.data => return 0,
+        Some('S') if x.data == &'S' => {
+            return 1;
+        }
+        _ => {}
+    };
+
+    let remaining = &s[1..];
+    let n = x.all_neighbors();
+    let v = vec![
+        n.north_west,
+        n.north,
+        n.north_east,
+        n.west,
+        n.east,
+        n.south_west,
+        n.south,
+        n.south_east,
+    ];
+
+    v.iter()
+        .enumerate()
+        .filter(|(d, _)| match dir {
+            None => true,
+            Some(x) => x == *d,
+        })
+        .filter_map(|(d, x)| x.map(|x| (d, x)))
+        .map(|(d, n)| spells(&n, remaining, Some(d)))
+        .sum()
 }
 
 fn problem2(_input: &Input) -> u32 {
@@ -35,7 +80,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(&input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 18)
     }
 
     #[test]
