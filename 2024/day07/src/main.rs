8197ab81691eae5a1b065aae733c2d44940f2de4
David Stone <david@stabbylambda.com>
2024-12-06 23:02:08 -0800

    2024 day 7 part 2

--- 2024/day07/src/main.rs
+++ 2024/day07/src/main.rs
@@ -1,5 +1,4 @@
-use core::num;
-use std::collections::VecDeque;
+use std::{collections::VecDeque, time::Instant};
 
 use nom::{
     bytes::complete::tag,
@@ -13,11 +12,15 @@ fn main() {
     let input = include_str!("../input.txt");
     let input = parse(input);
 
+    let i = Instant::now();
     let score = problem1(&input);
-    println!("problem 1 score: {score}");
+    let d = i.elapsed();
+    println!("problem 1 score: {score} in {d:?}");
 
+    let i = Instant::now();
     let score = problem2(&input);
-    println!("problem 2 score: {score}");
+    let d = i.elapsed();
+    println!("problem 2 score: {score} in {d:?}");
 }
 
 type Input = Vec<(u64, Vec<u64>)>;
@@ -31,23 +34,39 @@ fn parse(input: &str) -> Input {
     result.unwrap().1
 }
 
-fn problem1(input: &Input) -> u64 {
-    input
-        .iter()
-        .filter_map(|(test_value, elements)| is_valid(test_value, elements).then_some(test_value))
-        .sum()
+/// concat two numbers (e.g. concat_numbers(123, 45) == 12345)
+fn concat_numbers(a: u64, b: u64) -> u64 {
+    // figure out how many digits are in b by constantly dividing by 10 until we hit 0
+    let mut x = b;
+    let mut c = 0;
+    while x != 0 {
+        x /= 10;
+        c += 1;
+    }
+
+    // multiple a by 10^c to "shift" it over so we can concat with b by adding
+    a * 10_u64.pow(c) + b
 }
 
-fn is_valid(test_value: &u64, numbers: &[u64]) -> bool {
+fn is_valid(test_value: &u64, numbers: &[u64], allow_concat: bool) -> bool {
     let mut v = VecDeque::new();
     v.push_back((0, numbers[0]));
 
     while let Some((idx, current)) = v.pop_front() {
+        // all the operators cause the number to get bigger, bail if we go over
+        if current > *test_value {
+            continue;
+        }
+
         let new_idx = idx + 1;
         if let Some(new_num) = numbers.get(new_idx) {
             v.push_back((new_idx, current + new_num));
             v.push_back((new_idx, current * new_num));
+            if allow_concat {
+                v.push_back((new_idx, concat_numbers(current, *new_num)));
+            }
         } else if current == *test_value {
+            // if we're done with the numbers and we hit the test value, return true
             return true;
         }
     }
@@ -55,10 +74,23 @@ fn is_valid(test_value: &u64, numbers: &[u64]) -> bool {
     false
 }
 
-fn problem2(_input: &Input) -> u64 {
-    todo!()
+fn problem1(input: &Input) -> u64 {
+    input
+        .iter()
+        .filter_map(|(test_value, elements)| {
+            is_valid(test_value, elements, false).then_some(test_value)
+        })
+        .sum()
 }
 
+fn problem2(input: &Input) -> u64 {
+    input
+        .iter()
+        .filter_map(|(test_value, elements)| {
+            is_valid(test_value, elements, true).then_some(test_value)
+        })
+        .sum()
+}
 #[cfg(test)]
 mod test {
     use crate::{parse, problem1, problem2};
@@ -73,8 +105,8 @@ mod test {
     #[test]
     fn second() {
         let input = include_str!("../test.txt");
-        let input = parse(&input);
+        let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 11387)
     }
 }
