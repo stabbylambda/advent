03202e84d080a6de2bdbf57304ae863a55a68777
David Stone <david@stabbylambda.com>
2024-12-17 15:58:48 -0800

    New template

--- 2024/day18/src/main.rs
+++ 2024/day18/src/main.rs
@@ -1,29 +1,40 @@
-use nom::IResult;
+use nom::{
+    character::complete::{newline, u64},
+    multi::separated_list1,
+    IResult,
+};
+use std::time::Instant;
 
 fn main() {
     let input = include_str!("../input.txt");
-    let input = parse(&input);
+    let input = parse(input);
 
+    let i = Instant::now();
     let score = problem1(&input);
-    println!("problem 1 score: {score}");
+    let d = i.elapsed();
+    println!("problem 1 score: {score} in {d:?}");
 
+    let i = Instant::now();
     let score = problem2(&input);
-    println!("problem 2 score: {score}");
+    let d = i.elapsed();
+    println!("problem 2 score: {score} in {d:?}");
 }
 
-type Input = Vec<u32>;
+type Input = Vec<u64>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = todo!();
+    let result: IResult<&str, Input> = separated_list1(newline, u64)(input);
 
     result.unwrap().1
 }
 
-fn problem1(_input: &Input) -> u32 {
+fn problem1(input: &Input) -> u64 {
+    dbg!(input);
     todo!()
 }
 
-fn problem2(_input: &Input) -> u32 {
+fn problem2(input: &Input) -> u64 {
+    dbg!(input);
     todo!()
 }
 
@@ -31,18 +42,21 @@ fn problem2(_input: &Input) -> u32 {
 mod test {
     use crate::{parse, problem1, problem2};
     #[test]
+    #[ignore]
     fn first() {
         let input = include_str!("../test.txt");
-        let input = parse(&input);
+        let input = parse(input);
         let result = problem1(&input);
         assert_eq!(result, 0)
     }
 
     #[test]
+    #[ignore]
     fn second() {
         let input = include_str!("../test.txt");
-        let input = parse(&input);
+        let input = parse(input);
         let result = problem2(&input);
         assert_eq!(result, 0)
     }
 }
+
