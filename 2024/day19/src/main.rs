c58a4ec4b0ae274d22e2d0e05f8f46811330883b
David Stone <david@stabbylambda.com>
2024-12-18 23:21:57 -0800

    2024 day 19 part 1

--- 2024/day19/src/main.rs
+++ 2024/day19/src/main.rs
@@ -1,9 +1,12 @@
+use itertools::Itertools;
 use nom::{
-    character::complete::{newline, u64},
+    bytes::complete::tag,
+    character::complete::{alpha1, newline},
     multi::separated_list1,
+    sequence::separated_pair,
     IResult,
 };
-use std::time::Instant;
+use std::{collections::HashMap, mem, time::Instant};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -20,21 +23,49 @@ fn main() {
     println!("problem 2 score: {score} in {d:?}");
 }
 
-type Input = Vec<u64>;
+type Input<'a> = (Vec<&'a str>, Vec<&'a str>);
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(newline, u64)(input);
+    let result: IResult<&str, Input> = separated_pair(
+        separated_list1(tag(", "), alpha1),
+        tag("\n\n"),
+        separated_list1(newline, alpha1),
+    )(input);
 
     result.unwrap().1
 }
 
-fn problem1(input: &Input) -> u64 {
-    dbg!(input);
-    todo!()
+fn is_valid<'a>(towel: &'a str, designs: &[&str], memo: &mut HashMap<&'a str, bool>) -> bool {
+    if let Some(valid) = memo.get(towel) {
+        return *valid;
+    }
+
+    designs.iter().any(|design| {
+        towel
+            .strip_prefix(design)
+            .map(|rest| {
+                let valid = is_valid(rest, designs, memo);
+                memo.insert(rest, valid);
+                valid
+            })
+            .unwrap_or_default()
+    })
+}
+
+fn problem1(input: &Input) -> usize {
+    let (designs, towels) = input;
+
+    // start a dynamic programming cache with the empty string being valid
+    let mut memo: HashMap<&str, bool> = HashMap::new();
+    memo.insert("", true);
+
+    towels
+        .iter()
+        .filter(|x| is_valid(x, &designs[..], &mut memo))
+        .count()
 }
 
 fn problem2(input: &Input) -> u64 {
-    dbg!(input);
     todo!()
 }
 
@@ -42,12 +73,11 @@ fn problem2(input: &Input) -> u64 {
 mod test {
     use crate::{parse, problem1, problem2};
     #[test]
-    #[ignore]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 6)
     }
 
     #[test]
@@ -59,4 +89,3 @@ mod test {
         assert_eq!(result, 0)
     }
 }
-
