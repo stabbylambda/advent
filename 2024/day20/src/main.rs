5fa7daf8296cd800811017f609448bde9cce1fc2
David Stone <david@stabbylambda.com>
2024-12-19 22:30:28 -0800

    2024 day 20 part 1

--- 2024/day20/src/main.rs
+++ 2024/day20/src/main.rs
@@ -1,9 +1,19 @@
+use common::{
+    grid::{CardinalDirection, Coord, Grid},
+    nom::parse_grid,
+};
+use itertools::all;
 use nom::{
-    character::complete::{newline, u64},
+    branch::alt,
+    character::complete::{char, newline, u64},
+    combinator::map,
     multi::separated_list1,
     IResult,
 };
-use std::time::Instant;
+use std::{
+    collections::{BTreeMap, BTreeSet, VecDeque},
+    time::Instant,
+};
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -20,21 +30,95 @@ fn main() {
     println!("problem 2 score: {score} in {d:?}");
 }
 
-type Input = Vec<u64>;
+type Input = Grid<Tile>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(newline, u64)(input);
+    let result: IResult<&str, Input> = parse_grid(alt((
+        map(char('.'), |_| Tile::Track),
+        map(char('S'), |_| Tile::Start),
+        map(char('E'), |_| Tile::End),
+        map(char('#'), |_| Tile::Wall),
+    )))(input);
 
     result.unwrap().1
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Tile {
+    Track,
+    Start,
+    End,
+    Wall,
+}
+
+fn get_base_costs(input: &Input) -> BTreeMap<Coord, u64> {
+    let end = input.find(Tile::End).map(|x| x.coords).unwrap();
+
+    let mut base_cost: BTreeMap<Coord, u64> = BTreeMap::new();
+    let mut queue: VecDeque<(Coord, u64)> = VecDeque::from_iter([(end, 0)]);
+
+    while let Some((current, picoseconds)) = queue.pop_front() {
+        base_cost.insert(current, picoseconds);
+
+        for next in input.get(current).neighbors().iter() {
+            if (next.data == &Tile::Track || next.data == &Tile::Start)
+                && !base_cost.contains_key(&next.coords)
+            {
+                queue.push_back((next.coords, picoseconds + 1));
+            }
+        }
+    }
+
+    base_cost
+}
+
 fn problem1(input: &Input) -> u64 {
-    dbg!(input);
-    todo!()
+    let base_costs = get_base_costs(input);
+
+    let mut all_saved = BTreeMap::new();
+
+    for current in input
+        .iter()
+        .filter(|x| x.data == &Tile::Track || x.data == &Tile::Start)
+    {
+        for dir in [
+            CardinalDirection::North,
+            CardinalDirection::South,
+            CardinalDirection::East,
+            CardinalDirection::West,
+        ] {
+            // get neighbors that are walls
+            let Some(wall_neighbor) = input
+                .get_neighbor(current.coords, dir)
+                .filter(|x| x.data == &Tile::Wall)
+            else {
+                continue;
+            };
+
+            let Some(over_the_wall) = input
+                .get_neighbor(wall_neighbor.coords, dir)
+                .filter(|x| x.data == &Tile::Track || x.data == &Tile::End)
+            else {
+                continue;
+            };
+
+            let saved = (base_costs[&current.coords] as isize)
+                - (base_costs[&over_the_wall.coords] as isize)
+                - 2;
+
+            if saved > 0 {
+                all_saved
+                    .entry(saved as usize)
+                    .and_modify(|x| *x += 1)
+                    .or_insert(1);
+            }
+        }
+    }
+
+    all_saved.iter().filter(|x| x.0 >= &100).map(|x| x.1).sum()
 }
 
 fn problem2(input: &Input) -> u64 {
-    dbg!(input);
     todo!()
 }
 
@@ -42,7 +126,6 @@ fn problem2(input: &Input) -> u64 {
 mod test {
     use crate::{parse, problem1, problem2};
     #[test]
-    #[ignore]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
@@ -59,4 +142,3 @@ mod test {
         assert_eq!(result, 0)
     }
 }
-
