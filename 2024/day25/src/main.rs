a992b0f965b344400082748c4f8fb251d126c7e6
David Stone <david@stabbylambda.com>
2024-12-24 22:51:37 -0800

    2024 Merry Christmas!

--- 2024/day25/src/main.rs
+++ 2024/day25/src/main.rs
@@ -1,7 +1,6 @@
+use common::{grid::Grid, nom::parse_grid};
 use nom::{
-    character::complete::{newline, u64},
-    multi::separated_list1,
-    IResult,
+    branch::alt, bytes::complete::tag, character::complete::char, multi::separated_list1, IResult,
 };
 use std::time::Instant;
 
@@ -13,50 +12,60 @@ fn main() {
     let score = problem1(&input);
     let d = i.elapsed();
     println!("problem 1 score: {score} in {d:?}");
-
-    let i = Instant::now();
-    let score = problem2(&input);
-    let d = i.elapsed();
-    println!("problem 2 score: {score} in {d:?}");
 }
 
-type Input = Vec<u64>;
+type Input = Vec<Grid<char>>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(newline, u64)(input);
+    let result: IResult<&str, Input> =
+        separated_list1(tag("\n\n"), parse_grid(alt((char('#'), char('.')))))(input);
 
     result.unwrap().1
 }
 
-fn problem1(input: &Input) -> u64 {
-    dbg!(input);
-    todo!()
+fn problem1(input: &Input) -> usize {
+    let mut keys = vec![];
+    let mut locks = vec![];
+    for grid in input {
+        let is_lock = grid.points.first().unwrap().iter().all(|x| *x == '#');
+        let grid = grid.transpose();
+        let c: Vec<usize> = grid
+            .points
+            .iter()
+            .map(|r| {
+                r.iter()
+                    .filter(|x| if is_lock { **x == '.' } else { **x == '#' })
+                    .count()
+                    - 1
+            })
+            .collect();
+
+        if is_lock {
+            locks.push(c);
+        } else {
+            keys.push(c);
+        }
+    }
+
+    locks
+        .iter()
+        .flat_map(|lock| keys.iter().map(move |key| (lock, key)))
+        .filter(|(lock, key)| fits(&key[..], &lock[..]))
+        .count()
 }
 
-fn problem2(input: &Input) -> u64 {
-    dbg!(input);
-    todo!()
+fn fits(key: &[usize], lock: &[usize]) -> bool {
+    lock.iter().zip(key).all(|(l, k)| l >= k)
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{parse, problem1, problem2};
+    use crate::{parse, problem1};
     #[test]
-    #[ignore]
     fn first() {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0)
-    }
-
-    #[test]
-    #[ignore]
-    fn second() {
-        let input = include_str!("../test.txt");
-        let input = parse(input);
-        let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 3)
     }
 }
-
