4acfd0da306e91f3e86a4369a7a3059fde436ecd
David Stone <david@stabbylambda.com>
2025-12-06 09:45:09 -0800

    2025 day 5 part 2

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -1,6 +1,14 @@
+use std::ops::RangeInclusive;
+
 use nom::{
-    character::complete::{i32, newline},
+    bytes::complete::tag,
+    character::{
+        complete::{char, i32, newline},
+        streaming::u64,
+    },
+    combinator::map,
     multi::separated_list1,
+    sequence::separated_pair,
     IResult, Parser,
 };
 
@@ -15,16 +23,34 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<i32>;
+type Input = Inventory;
+struct Inventory {
+    ranges: Vec<RangeInclusive<u64>>,
+    items: Vec<u64>,
+}
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(newline, i32).parse(input);
+    let result: IResult<&str, Input> = map(
+        separated_pair(
+            separated_list1(
+                newline,
+                map(separated_pair(u64, char('-'), u64), |(a, b)| a..=b),
+            ),
+            tag("\n\n"),
+            separated_list1(newline, u64),
+        ),
+        |(ranges, items)| Inventory { ranges, items },
+    )
+    .parse(input);
 
     result.unwrap().1
 }
 
-fn problem1(x: &Input) -> u32 {
-    todo!()
+fn problem1(x: &Input) -> usize {
+    x.items
+        .iter()
+        .filter(|i| x.ranges.iter().any(|r| r.contains(i)))
+        .count()
 }
 
 fn problem2(x: &Input) -> u32 {
@@ -39,7 +65,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 3);
     }
 
     #[test]
