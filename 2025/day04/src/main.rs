5052ad7985b762fc9c3d0d634dc2ec3ab4650a49
David Stone <david@stabbylambda.com>
2025-12-06 09:24:16 -0800

    2025 day 4 part 1

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -1,8 +1,15 @@
+use std::fmt::Debug;
+
 use nom::{
-    character::complete::{i32, newline},
+    branch::alt,
+    character::complete::{char, i32, newline},
+    combinator::map,
     multi::separated_list1,
     IResult, Parser,
 };
+
+use common::grid::{Grid, GridSquare, HasNeighbors};
+use common::nom::parse_grid;
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -15,20 +22,80 @@ fn main() {
     println!("problem 2 score: {score}");
 }
 
-type Input = Vec<i32>;
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum Tile {
+    Roll,
+    Empty,
+}
+
+impl Debug for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Roll => write!(f, "@"),
+            Self::Empty => write!(f, "."),
+        }
+    }
+}
+
+impl std::fmt::Display for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Tile::Roll => write!(f, "@"),
+            Tile::Empty => write!(f, "."),
+        }
+    }
+}
+
+type Input = Grid<Tile>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(newline, i32).parse(input);
+    let result: IResult<&str, Input> = parse_grid(alt((
+        map(char('@'), |_| Tile::Roll),
+        map(char('.'), |_| Tile::Empty),
+    )))
+    .parse(input);
 
     result.unwrap().1
 }
 
-fn problem1(x: &Input) -> u32 {
-    todo!()
+fn removable<'a>(grid: &'a Grid<Tile>) -> Vec<GridSquare<'a, Tile>> {
+    let v = grid
+        .iter()
+        .filter(|x| x.data == &Tile::Roll)
+        .filter(|x| {
+            let rolls = x
+                .all_neighbors()
+                .iter()
+                .filter(|n| n.data == &Tile::Roll)
+                .count();
+            rolls < 4
+        })
+        .collect::<Vec<_>>();
+
+    v
 }
 
-fn problem2(x: &Input) -> u32 {
-    todo!()
+fn problem1(x: &Input) -> usize {
+    removable(x).len()
+}
+
+fn problem2(x: &Input) -> usize {
+    let mut grid = x.clone();
+    let mut removed = 0;
+    loop {
+        let r = removable(&grid);
+        let count = r.len();
+        if count == 0 {
+            return removed;
+        }
+
+        let mut new_grid = grid.clone();
+        for x in r {
+            grid.get(x.coords).data = &Tile::Empty;
+        }
+
+        removed += count;
+    }
 }
 
 #[cfg(test)]
@@ -39,7 +106,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 13);
     }
 
     #[test]
