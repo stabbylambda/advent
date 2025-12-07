5b90c1ded52ec54c7aed33a96dda48a00c36e3a8
David Stone <david@stabbylambda.com>
2025-12-06 21:54:50 -0800

    2025 day 7 part 1

--- 2025/day07/src/main.rs
+++ 2025/day07/src/main.rs
@@ -1,9 +1,12 @@
-use common::{answer, read_input};
-use nom::{
-    character::complete::{i32, newline},
-    multi::separated_list1,
-    IResult, Parser,
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
+
+use common::{
+    answer,
+    grid::{CardinalDirection, Grid},
+    nom::parse_grid,
+    read_input,
 };
+use nom::{branch::alt, character::complete::char, combinator::map, IResult, Parser};
 
 fn main() {
     let input = read_input!();
@@ -13,16 +16,59 @@ fn main() {
     answer!(problem2(&input));
 }
 
-type Input = Vec<i32>;
+type Input = Grid<Tile>;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord)]
+enum Tile {
+    Start,
+    Splitter,
+    Empty,
+}
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(newline, i32).parse(input);
+    let result: IResult<&str, Input> = parse_grid(alt((
+        map(char('S'), |_| Tile::Start),
+        map(char('.'), |_| Tile::Empty),
+        map(char('^'), |_| Tile::Splitter),
+    )))
+    .parse(input);
 
     result.unwrap().1
 }
 
-fn problem1(x: &Input) -> u32 {
-    todo!()
+fn problem1(x: &Input) -> usize {
+    let start = x.iter().find(|x| x.data == &Tile::Start).unwrap();
+
+    let mut splits: BTreeSet<(usize, usize)> = BTreeSet::new();
+    let mut queue = VecDeque::new();
+    queue.push_back(start);
+
+    while let Some(current) = queue.pop_front() {
+        let Some(next) = current.get_neighbor(CardinalDirection::South) else {
+            continue;
+        };
+
+        if splits.contains(&next.coords) {
+            continue;
+        }
+
+        match next.data {
+            Tile::Empty => queue.push_back(next),
+            Tile::Splitter => {
+                splits.insert(next.coords);
+
+                if let Some(east) = next.get_neighbor(CardinalDirection::East) {
+                    queue.push_back(east);
+                }
+                if let Some(west) = next.get_neighbor(CardinalDirection::West) {
+                    queue.push_back(west);
+                }
+            }
+            Tile::Start => unreachable!(),
+        }
+    }
+
+    splits.len()
 }
 
 fn problem2(x: &Input) -> u32 {
@@ -37,7 +83,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 21);
     }
 
     #[test]
