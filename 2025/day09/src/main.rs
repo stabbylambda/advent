94d9062ff511c53c74c4fe83af451591cde73543
David Stone <david@stabbylambda.com>
2025-12-09 14:28:54 -0800

    2025 day 9 part 2

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -1,4 +1,4 @@
-use common::{answer, grid::Grid, read_input};
+use common::{answer, read_input};
 use itertools::Itertools;
 use nom::{
     bytes::tag,
@@ -25,24 +25,79 @@ fn parse(input: &str) -> Input {
     result.unwrap().1
 }
 
+struct Rectangle {
+    min: (usize, usize),
+    max: (usize, usize),
+}
+
+impl Rectangle {
+    fn new(a: (usize, usize), b: (usize, usize)) -> Self {
+        let (ax, ay) = a;
+        let (bx, by) = b;
+
+        let (min_x, max_x) = (ax.min(bx), ax.max(bx));
+        let (min_y, max_y) = (ay.min(by), ay.max(by));
+
+        Self {
+            min: (min_x, min_y),
+            max: (max_x, max_y),
+        }
+    }
+
+    fn area(&self) -> usize {
+        let (ax, ay) = self.min;
+        let (bx, by) = self.max;
+
+        let x = ax.abs_diff(bx) + 1;
+        let y = ay.abs_diff(by) + 1;
+
+        x * y
+    }
+
+    fn enclosed_in(&self, edges: &[Rectangle]) -> bool {
+        let (txmin, tymin) = &self.min;
+        let (txmax, tymax) = &self.max;
+
+        !edges.iter().any(|r| {
+            let (exmin, eymin) = &r.min;
+            let (exmax, eymax) = &r.max;
+
+            exmax > txmin && exmin < txmax && eymax > tymin && eymin < tymax
+        })
+    }
+}
+
 fn problem1(x: &Input) -> usize {
     x.iter()
         .combinations(2)
-        .map(|x| {
-            let (ax, ay) = *x[0];
-            let (bx, by) = *x[1];
-
-            let x = ax.abs_diff(bx) + 1;
-            let y = ay.abs_diff(by) + 1;
-
-            x * y
-        })
+        .map(|x| Rectangle::new(*x[0], *x[1]))
+        .map(|r| r.area())
         .max()
         .unwrap()
 }
 
-fn problem2(x: &Input) -> u32 {
-    todo!()
+fn problem2(x: &Input) -> usize {
+    let mut x = x.clone();
+    x.push(x[0]);
+
+    let edges = x
+        .windows(2)
+        .map(|pair| Rectangle::new(pair[0], pair[1]))
+        .collect_vec();
+
+    let tiles = x
+        .iter()
+        .combinations(2)
+        .map(|x| Rectangle::new(*x[0], *x[1]))
+        .collect_vec();
+
+    let result = tiles
+        .iter()
+        .filter_map(|t| t.enclosed_in(&edges).then(|| t.area()))
+        .max()
+        .unwrap();
+
+    result
 }
 
 #[cfg(test)]
@@ -61,6 +116,6 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 24)
     }
 }
