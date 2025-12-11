eb0e596be100b54e3afb10e5a76b450e55e486a1
David Stone <david@stabbylambda.com>
2025-12-10 22:37:47 -0800

    2025 day 11 part 1

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -1,7 +1,15 @@
-use common::{answer, read_input};
+use std::collections::{HashMap, VecDeque};
+
+use common::{answer, read_input, to_number};
 use nom::{
-    character::complete::{i32, newline},
+    bytes::complete::tag,
+    character::{
+        complete::{i32, newline},
+        streaming::alpha1,
+    },
+    combinator::map,
     multi::separated_list1,
+    sequence::{delimited, separated_pair},
     IResult, Parser,
 };
 
@@ -13,16 +21,39 @@ fn main() {
     answer!(problem2(&input));
 }
 
-type Input = Vec<i32>;
+type Input<'a> = HashMap<&'a str, Vec<&'a str>>;
 
 fn parse(input: &str) -> Input {
-    let result: IResult<&str, Input> = separated_list1(newline, i32).parse(input);
+    let result: IResult<&str, Input> = map(
+        separated_list1(
+            newline,
+            separated_pair(alpha1, tag(": "), separated_list1(tag(" "), alpha1)),
+        ),
+        |x| HashMap::from_iter(x),
+    )
+    .parse(input);
 
     result.unwrap().1
 }
 
 fn problem1(x: &Input) -> u32 {
-    todo!()
+    let state = "you";
+    let mut queue = VecDeque::new();
+    queue.push_back(state);
+    let mut count = 0;
+
+    while let Some(state) = queue.pop_front() {
+        if state == "out" {
+            count += 1;
+            continue;
+        }
+
+        for next in &x[state] {
+            queue.push_back(next);
+        }
+    }
+
+    count
 }
 
 fn problem2(x: &Input) -> u32 {
@@ -37,7 +68,7 @@ mod test {
         let input = include_str!("../test.txt");
         let input = parse(input);
         let result = problem1(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 5);
     }
 
     #[test]
