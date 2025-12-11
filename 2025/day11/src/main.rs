f454bca4c8c8d6916fe12658f8e47609e180f4a0
David Stone <david@stabbylambda.com>
2025-12-10 23:42:13 -0800

    2025 day 11 part 2

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -1,15 +1,12 @@
-use std::collections::{HashMap, VecDeque};
+use std::{collections::HashMap, hash::Hash};
 
-use common::{answer, read_input, to_number};
+use common::{answer, read_input};
 use nom::{
     bytes::complete::tag,
-    character::{
-        complete::{i32, newline},
-        streaming::alpha1,
-    },
+    character::{complete::newline, streaming::alpha1},
     combinator::map,
     multi::separated_list1,
-    sequence::{delimited, separated_pair},
+    sequence::separated_pair,
     IResult, Parser,
 };
 
@@ -23,7 +20,7 @@ fn main() {
 
 type Input<'a> = HashMap<&'a str, Vec<&'a str>>;
 
-fn parse(input: &str) -> Input {
+fn parse<'a>(input: &'a str) -> Input<'a> {
     let result: IResult<&str, Input> = map(
         separated_list1(
             newline,
@@ -36,28 +33,62 @@ fn parse(input: &str) -> Input {
     result.unwrap().1
 }
 
-fn problem1(x: &Input) -> u32 {
-    let state = "you";
-    let mut queue = VecDeque::new();
-    queue.push_back(state);
-    let mut count = 0;
+#[derive(Clone, Eq, PartialEq, Hash)]
+struct State<'a> {
+    node: &'a str,
+    dac: bool,
+    fft: bool,
+}
 
-    while let Some(state) = queue.pop_front() {
-        if state == "out" {
-            count += 1;
-            continue;
-        }
-
-        for next in &x[state] {
-            queue.push_back(next);
+impl<'a> State<'a> {
+    fn new(node: &'a str) -> State<'a> {
+        State {
+            node,
+            dac: false,
+            fft: false,
         }
     }
 
-    count
+    fn move_to(&self, node: &'a str) -> State<'a> {
+        State {
+            node,
+            dac: self.dac || node == "dac",
+            fft: self.fft || node == "fft",
+        }
+    }
 }
 
-fn problem2(x: &Input) -> u32 {
-    todo!()
+fn paths<'a, F>(state: State<'a>, x: &'a Input, cache: &mut HashMap<State<'a>, u64>, f: F) -> u64
+where
+    F: Copy + Clone + Fn(&State) -> bool,
+{
+    if let Some(result) = cache.get(&state) {
+        return *result;
+    }
+
+    if state.node == "out" {
+        return f(&state).into();
+    }
+
+    x[state.node]
+        .iter()
+        .map(|n| {
+            let state = state.move_to(n);
+            let result = paths(state.clone(), x, cache, f);
+            cache.insert(state, result);
+            result
+        })
+        .sum()
+}
+
+fn problem1(x: &Input) -> u64 {
+    paths(State::new("you"), x, &mut HashMap::new(), |_| true)
+}
+
+fn problem2(x: &Input) -> u64 {
+    paths(State::new("svr"), x, &mut HashMap::new(), |s| {
+        s.dac && s.fft
+    })
 }
 
 #[cfg(test)]
@@ -73,9 +104,9 @@ mod test {
 
     #[test]
     fn second() {
-        let input = include_str!("../test.txt");
+        let input = include_str!("../test2.txt");
         let input = parse(input);
         let result = problem2(&input);
-        assert_eq!(result, 0)
+        assert_eq!(result, 2)
     }
 }
